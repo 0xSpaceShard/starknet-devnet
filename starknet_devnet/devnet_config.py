0651dcec9adab994d84d018fe5298f4ad56f559f
mikiw <mikiw.pp@gmail.com>
2022-09-19 11:36:39 +0200

    fix formatting

--- starknet_devnet/devnet_config.py
+++ starknet_devnet/devnet_config.py
@@ -107,7 +107,8 @@ def parse_args(raw_args: List[str]):
     parser.add_argument(
         "--lite-mode",
         action="store_true",
-        help="Introduces speed-up by skipping block hash and deploy transaction hash calculation - applies sequential numbering instead (0x0, 0x1, 0x2, ...).",
+        help="Introduces speed-up by skipping block hash and deploy transaction hash calculation"
+        " - applies sequential numbering instead (0x0, 0x1, 0x2, ...).",
     )
     parser.add_argument(
         "--accounts",
