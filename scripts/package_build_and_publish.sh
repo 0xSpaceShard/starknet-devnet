460b12b9b82104381477b9d4fc18afc57a9c14cc
FabijanC <fabijan.corak@gmail.com>
2022-11-30 14:51:20 +0100

    Add automatic docs deployment [skip ci]

--- scripts/package_build_and_publish.sh
+++ scripts/package_build_and_publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eu
 
 [ -f .env ] && source .env
 
@@ -17,4 +17,18 @@ if [ "$PYPI_VERSION" = "$LOCAL_VERSION" ]; then
     echo "Publishing skipped"
 else
     poetry publish --username "$PYPI_USER" --password "$PYPI_PASS"
+
+    # publish docs
+
+    cd page && npm ci
+
+    export GIT_USER=FabijanC # exporting to be visible in npm run deploy
+    git config --global user.email "$GIT_USER@users.noreply.github.com"
+    git config --global user.name "$GIT_USER"
+    echo "machine github.com login $GIT_USER password $GITHUB_TOKEN" > ~/.netrc
+
+    # skip ci to avoid gh-pages erroring on circleci
+    LATEST_COMMIT_HASH=$(git rev-parse HEAD)
+    CUSTOM_COMMIT_MESSAGE="Deploy website - based on $LATEST_COMMIT_HASH [skip ci]" \
+        npm run deploy
 fi
