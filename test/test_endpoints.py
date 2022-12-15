e1b2f1ecdbdfccee04592077d2fbb241be9cf982
mikiw <mikiw.pp@gmail.com>
2022-12-15 10:31:16 +0100

    Add constants

--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -41,7 +41,12 @@ CALL_CONTENT = load_file_content("call.json")
 INVALID_HASH = "0x58d4d4ed7580a7a98ab608883ec9fe722424ce52c19f2f369eeea301f535914"
 INVALID_ADDRESS = "0x123"
 USER_ID = 1
-
+L1_CONTRACT_ADDRESS = 0xE7F1725E7734CE288F8367E1BB143E90BB3F0512
+L2_CONTRACT_ADDRESS = "0x00285ddb7e5c777b310d806b9b2a0f7c7ba0a41f12b420219209d97a3b7f25b2"
+ENTRY_POINT_SELECTOR = "0xC73F681176FC7B3F9693986FD7B14581E8D540519E27400E88B8713932BE01"
+CONSUME_PAYLOAD = ["0x0", "0x1", "0x3e8"]
+MESSAGE_TO_l2_PAYLOAD = ["0x1", "0x1"]
+MESSAGE_TO_l2_NONCE = "0x0"
 
 def send_transaction(req_dict: dict):
     """Sends the dict in a POST request and returns the response data."""
@@ -393,10 +398,10 @@ def test_send_message_to_l2_deploy_execute():
     response = send_message_to_l2(
         {
             "l2_contract_address": deploy_info["address"],
-            "entry_point_selector": "0xC73F681176FC7B3F9693986FD7B14581E8D540519E27400E88B8713932BE01",
-            "l1_contract_address": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-            "payload": ["0x1", "0x1"],
-            "nonce": "0x0",
+            "entry_point_selector": ENTRY_POINT_SELECTOR,
+            "l1_contract_address": str(L1_CONTRACT_ADDRESS),
+            "payload": MESSAGE_TO_l2_PAYLOAD,
+            "nonce": MESSAGE_TO_l2_NONCE,
         }
     )
 
@@ -407,7 +412,6 @@ def test_send_message_to_l2_deploy_execute():
         abi_path=L1L2_ABI_PATH,
         inputs=[str(USER_ID)],
     )
-
     assert int(value) == 1
     assert response.status_code == 200
     assert_tx_status(hex(response.json().get("transaction_hash")), "ACCEPTED_ON_L2")
@@ -437,11 +441,11 @@ def test_send_message_to_l2_execute_without_deploy():
     # Create l1 to l2 mock transaction
     response = send_message_to_l2(
         {
-            "l2_contract_address": "0x00285ddb7e5c777b310d806b9b2a0f7c7ba0a41f12b420219209d97a3b7f25b2",
-            "entry_point_selector": "0xC73F681176FC7B3F9693986FD7B14581E8D540519E27400E88B8713932BE01",
-            "l1_contract_address": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-            "payload": ["0x1", "0x1"],
-            "nonce": "0x0",
+            "l2_contract_address": L2_CONTRACT_ADDRESS,
+            "entry_point_selector": ENTRY_POINT_SELECTOR,
+            "l1_contract_address": str(L1_CONTRACT_ADDRESS),
+            "payload": MESSAGE_TO_l2_PAYLOAD,
+            "nonce": MESSAGE_TO_l2_NONCE,
         }
     )
 
@@ -475,8 +479,8 @@ def test_consume_message_from_l2_deploy_execute():
     response = consume_message_from_l2(
         {
             "l2_contract_address": deploy_info["address"],
-            "l1_contract_address": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-            "payload": ["0x0", "0x1", "0x3e8"],
+            "l1_contract_address": str(L1_CONTRACT_ADDRESS),
+            "payload": CONSUME_PAYLOAD,
         }
     )
 
@@ -490,8 +494,8 @@ def test_consume_message_from_l2_deploy_execute_without_withdraw():
     response = consume_message_from_l2(
         {
             "l2_contract_address": deploy_info["address"],
-            "l1_contract_address": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-            "payload": ["0x0", "0x1", "0x3e8"],
+            "l1_contract_address": str(L1_CONTRACT_ADDRESS),
+            "payload": CONSUME_PAYLOAD,
         }
     )
 
@@ -518,9 +522,9 @@ def test_consume_message_from_l2_execute_without_deploy():
     """Test POST l2 to l1 without contract deploy"""
     response = consume_message_from_l2(
         {
-            "l2_contract_address": "0x00285ddb7e5c777b310d806b9b2a0f7c7ba0a41f12b420219209d97a3b7f25b2",
-            "l1_contract_address": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-            "payload": ["0x0", "0x1", "0x3e8"],
+            "l2_contract_address": L2_CONTRACT_ADDRESS,
+            "l1_contract_address": str(L1_CONTRACT_ADDRESS),
+            "payload": CONSUME_PAYLOAD,
         }
     )
 
