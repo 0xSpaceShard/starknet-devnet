a031522821fb53c7e41cc1a6dd9a8e9e0eb25497
FabijanC <fabijan.corak@gmail.com>
2022-12-23 13:21:51 +0100

    Improve cairo vm testing

--- test/test_cairo_vm.py
+++ test/test_cairo_vm.py
@@ -10,10 +10,18 @@ from .util import DevnetBackgroundProc, read_stream, terminate_and_wait
 ACTIVE_DEVNET = DevnetBackgroundProc()
 
 _VM_VAR = "STARKNET_DEVNET_CAIRO_VM"
+_RUST_VM_LOG_LINE = "Using Cairo VM: Rust"
 
 
-@pytest.mark.parametrize("cairo_vm", ["", "python", "rust"])
-def test_valid_cairo_vm(cairo_vm):
+@pytest.mark.parametrize(
+    "cairo_vm, assert_rust_vm_logged",
+    [
+        ("", False),
+        ("python", False),
+        ("rust", True),
+    ],
+)
+def test_valid_cairo_vm(cairo_vm, assert_rust_vm_logged):
     """Test if the invalid chain id fails"""
 
     env_copy = os.environ.copy()
@@ -21,6 +29,13 @@ def test_valid_cairo_vm(cairo_vm):
 
     proc = ACTIVE_DEVNET.start(stderr=subprocess.PIPE, env=env_copy)
     terminate_and_wait(proc)
+
+    stderr = proc.stderr.read().decode("utf-8")
+    if assert_rust_vm_logged:
+        assert _RUST_VM_LOG_LINE in stderr
+    else:
+        assert _RUST_VM_LOG_LINE not in stderr
+
     assert proc.returncode == 0
 
 
@@ -32,6 +47,8 @@ def test_passing_if_no_cairo_vm_set():
 
     proc = ACTIVE_DEVNET.start(stderr=subprocess.PIPE, env=env_copy)
     terminate_and_wait(proc)
+
+    assert _RUST_VM_LOG_LINE not in proc.stderr.read().decode("utf-8")
     assert proc.returncode == 0
 
 
