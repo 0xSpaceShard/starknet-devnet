f61060a3d576ce553db9ab0bbad7421fdadad656
mikiw <mikiw.pp@gmail.com>
2022-12-30 10:28:08 +0100

    Update test_blocks_on_demand.py
    [skip ci]

--- test/test_blocks_on_demand.py
+++ test/test_blocks_on_demand.py
@@ -5,29 +5,45 @@ Test blocks on demand mode.
 import requests
 from test.rpc.rpc_utils import gateway_call
 
+from .account import invoke
 from .settings import APP_URL
 from .shared import (
     CONTRACT_PATH,
     PREDEPLOY_ACCOUNT_CLI_ARGS,
+    PREDEPLOYED_ACCOUNT_ADDRESS,
+    PREDEPLOYED_ACCOUNT_PRIVATE_KEY,
 )
 from .util import (
     deploy,
     devnet_in_background,
 )
 
+CONTRAC_HASH_CLASS = "0x71df7c871d389943e24aaaf85d41594266d12f2f9b580a9f92ba4a0bf763d67"
+
+
 @devnet_in_background(*[*PREDEPLOY_ACCOUNT_CLI_ARGS,"--blocks-on-demand",])
 def test_blocks_on_demand():
     """Test deploy in blocks on demand mode"""
     latest_block = gateway_call("get_block", blockNumber="latest")
-    block_number_before_deploy = latest_block["block_number"]
-    deploy(contract=CONTRACT_PATH)
+    genesis_block_number = latest_block["block_number"]
+
+    # Deploy and invoke
+    deploy_info = deploy(CONTRACT_PATH, inputs=["0"])
+    invoke(
+        calls=[(deploy_info["address"], "increase_balance", [10, 20])],
+        account_address=PREDEPLOYED_ACCOUNT_ADDRESS,
+        private_key=PREDEPLOYED_ACCOUNT_PRIVATE_KEY,
+    )
+
     latest_block = gateway_call("get_block", blockNumber="latest")
-    block_number_after_deploy = latest_block["block_number"]
+    block_number_after_deploy_and_invoke = latest_block["block_number"]
 
     requests.post(f"{APP_URL}/create_block_on_demand")
     latest_block = gateway_call("get_block", blockNumber="latest")
     block_number_after_block_on_demand_call = latest_block["block_number"]
 
-    assert block_number_before_deploy == 0
-    assert block_number_after_deploy == 0
+    assert genesis_block_number == 0
+    assert block_number_after_deploy_and_invoke == 0
     assert block_number_after_block_on_demand_call == 1
+    assert len(latest_block["transactions"]) == 2
+    assert latest_block["transactions"][0]["class_hash"] == CONTRAC_HASH_CLASS
