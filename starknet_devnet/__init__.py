3103a93df1c4f4b339c1e02fa657f8e821af7718
FabijanC <fabijan.corak@gmail.com>
2023-04-25 14:19:07 +0200

    Keep only starknet_pathfinder_crypto (rust)

--- starknet_devnet/__init__.py
+++ starknet_devnet/__init__.py
@@ -16,86 +16,37 @@ import sys
 __version__ = "0.5.0"
 
 
-def _patch_pedersen_hash():
-    """
-    This is a monkey-patch to improve the performance of the devnet
-    We are using c++ code for calculating the pedersen hashes
-    instead of python implementation from cairo-lang package
-    """
-
-    from crypto_cpp_py.cpp_bindings import cpp_hash as patched_pedersen_hash
-    import starkware.crypto.signature.fast_pedersen_hash
-
-    setattr(
-        sys.modules["starkware.crypto.signature.fast_pedersen_hash"],
-        "pedersen_hash",
-        patched_pedersen_hash,
-    )
-    setattr(
-        sys.modules["starkware.crypto.signature.fast_pedersen_hash"],
-        "pedersen_hash_func",
-        patched_pedersen_hash,
-    )
-
-    import starkware.cairo.lang.vm.crypto
-
-    setattr(
-        sys.modules["starkware.cairo.lang.vm.crypto"],
-        "pedersen_hash",
-        patched_pedersen_hash,
-    )
-
-
-_patch_pedersen_hash()
-
-
-def _patch_poseidon_hash():
-    """
-    Improves performance by substituting the default Python implementation of Poseidon hash
-    with swm's Python wrapper of a C implementation.
-    """
-
-    import starkware.cairo.common
-
-    from poseidon_py import poseidon_hash
-
-    # alternative, shorter approach
-    # sys.modules["starkware.cairo.common"].poseidon_hash = poseidon_hash
-
-    setattr(sys.modules["starkware.cairo.common"], "poseidon_hash", poseidon_hash)
-
-
-_patch_poseidon_hash()  # doesn't have any effect if line 49 is active
-
-
 def _patch_poseidon_hash_rust():
-    """Uses equilibriums rust implementation of poseidon"""
+    """
+    Monkey-patches pedersen and poseidon hashing functions
+    with Equilibrium's Rust implementation
+    """
 
     import starknet_pathfinder_crypto
-    import starkware.crypto.signature.fast_pedersen_hash
     import starkware.cairo.common.poseidon_hash
+    import starkware.crypto.signature.fast_pedersen_hash
 
-    starkware.crypto.signature.fast_pedersen_hash.pedersen_hash_func = (
-        starknet_pathfinder_crypto.pedersen_hash_func
+    starkware.crypto.signature.fast_pedersen_hash.pedersen_hash_func = getattr(
+        starknet_pathfinder_crypto, "pedersen_hash_func"
     )
-    starkware.crypto.signature.fast_pedersen_hash.pedersen_hash = (
-        starknet_pathfinder_crypto.pedersen_hash
+    starkware.crypto.signature.fast_pedersen_hash.pedersen_hash = getattr(
+        starknet_pathfinder_crypto, "pedersen_hash"
     )
-    starkware.cairo.common.poseidon_hash.poseidon_hash = (
-        starknet_pathfinder_crypto.poseidon_hash
+    starkware.cairo.common.poseidon_hash.poseidon_hash = getattr(
+        starknet_pathfinder_crypto, "poseidon_hash"
     )
-    starkware.cairo.common.poseidon_hash.poseidon_hash_func = (
-        starknet_pathfinder_crypto.poseidon_hash_func
+    starkware.cairo.common.poseidon_hash.poseidon_hash_func = getattr(
+        starknet_pathfinder_crypto, "poseidon_hash_func"
     )
-    starkware.cairo.common.poseidon_hash.poseidon_hash_many = (
-        starknet_pathfinder_crypto.poseidon_hash_many
+    starkware.cairo.common.poseidon_hash.poseidon_hash_many = getattr(
+        starknet_pathfinder_crypto, "poseidon_hash_many"
     )
-    starkware.cairo.common.poseidon_hash.poseidon_perm = (
-        starknet_pathfinder_crypto.poseidon_perm
+    starkware.cairo.common.poseidon_hash.poseidon_perm = getattr(
+        starknet_pathfinder_crypto, "poseidon_perm"
     )
 
 
-# _patch_poseidon_hash_rust()
+_patch_poseidon_hash_rust()
 
 
 def _patch_copy():
