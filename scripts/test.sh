d1a8fd58ac1300a9373d55566cb3187ed58f432a
FabijanC <fabijan.corak@gmail.com>
2023-04-26 13:05:33 +0200

    Use max 10 workers

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,6 @@ set -eu
 
 # Using dist=loadfile because currently some tests might have certain collisions
 # namely dump tests which do a clean up and potentially remove dumps created by other tests
-CMD="poetry run pytest --numprocesses=auto --maxprocesses=8 --dist=loadfile -vv ${1:-test/}"
+CMD="poetry run pytest --numprocesses=auto --maxprocesses=10 --dist=loadfile -vv ${1:-test/}"
 echo $CMD
 $CMD
