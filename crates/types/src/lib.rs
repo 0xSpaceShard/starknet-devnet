76b8d14bc3c2e253fb01a513803ec8515d7c54df
marioiordanov <mio@shardlabs.io>
2023-06-08 15:41:09 +0300

    reexport new dependencies from types crate

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -4,10 +4,12 @@ pub mod contract_class;
 pub mod contract_storage_key;
 pub mod error;
 pub mod felt;
+pub mod patricia_key;
+mod test_utils;
 pub mod traits;
 mod utils;
 
 pub type DevnetResult<T> = Result<T, crate::error::Error>;
 
 // Re export libraries
-pub use starknet_api;
+pub use {cairo_felt, num_bigint, num_integer, starknet_api};
