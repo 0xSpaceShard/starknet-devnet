4d259d11ba474b3c202cc43303257d7a9c38194b
marioiordanov <mio@shardlabs.io>
2023-06-08 10:26:07 +0300

    removed types folder from starknet crate

--- crates/starknet/src/types/contract_storage_key.rs
+++
@@ -1,4 +0,0 @@
-use super::{contract_address::ContractAddress, felt::Felt};
-
-#[derive(Debug, Default, Clone, Copy)]
-pub struct ContractStorageKey(ContractAddress, Felt);
--- crates/starknet/src/types/mod.rs
+++
@@ -1,8 +0,0 @@
-use crate::error::Error;
-
-pub(crate) mod contract_address;
-pub(crate) mod contract_class;
-pub(crate) mod contract_storage_key;
-pub(crate) mod felt;
-
-pub type DevnetResult<T> = Result<T, Error>;
