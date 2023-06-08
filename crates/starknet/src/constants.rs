394485e8c05e0279294b42aff19b68a8c3e14bed
marioiordanov <mio@shardlabs.io>
2023-06-08 15:43:55 +0300

    added contract classes as json files

--- crates/starknet/src/constants.rs
+++ crates/starknet/src/constants.rs
@@ -1,5 +1,25 @@
-pub(crate) const CAIRO_0_ACCOUNT_CONTRACT_PATH: &str =
-    concat!(env!("CARGO_MANIFEST_DIR"), "/accounts_artifacts/OpenZeppelin/0.5.1/Account.cairo/Account.json");
+pub(crate) const CAIRO_0_ACCOUNT_CONTRACT_PATH: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/accounts_artifacts/OpenZeppelin/0.5.1/Account.cairo/Account.json"
+);
 
 pub(crate) const CAIRO_0_ACCOUNT_CONTRACT_HASH: &str =
     "0x4d07e40e93398ed3c76981e72dd1fd22557a78ce36c0515f679e27f0bb5bc5f";
+
+pub(crate) const ERC20_OZ_ACCOUNT_PATH: &str =
+    concat!(env!("CARGO_MANIFEST_DIR"), "/accounts_artifacts/ERC20_Mintable_OZ_0.2.0.json");
+
+pub(crate) const ERC20_OZ_ACCOUNT_HASH: &str =
+    "0x6A22BF63C7BC07EFFA39A25DFBD21523D211DB0100A0AFD054D172B81840EAF";
+
+pub(crate) const ERC20_OZ_ACCOUNT_ADDRESS: &str =
+    "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7";
+
+pub(crate) const UDC_OZ_ACCOUNT_PATH: &str =
+    concat!(env!("CARGO_MANIFEST_DIR"), "/accounts_artifacts/UDC_OZ_0.5.0.json");
+
+pub(crate) const UDC_OZ_ACCOUNT_HASH: &str =
+    "0x7B3E05F48F0C69E4A65CE5E076A66271A527AFF2C34CE1083EC6E1526997A69";
+
+pub(crate) const UDC_OZ_ACCOUNT_ADDRESS: &str =
+    "0x41A78E741E5AF2FEC34B695679BC6891742439F7AFB8484ECD7766661AD02BF";
