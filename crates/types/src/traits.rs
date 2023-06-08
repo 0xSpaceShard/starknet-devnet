50ee808a03edbc0e9e59d91f2474fab1c27034ce
marioiordanov <mio@shardlabs.io>
2023-06-08 15:37:54 +0300

    removed traits not applicable to the types crate

--- crates/types/src/traits.rs
+++ crates/types/src/traits.rs
@@ -1,39 +1,9 @@
-use crate::{
-    contract_class::ContractClass,
-    error::Error,
-    felt::{ClassHash, Felt, Key},
-    DevnetResult,
-};
+use crate::felt::Felt;
+use crate::DevnetResult;
 
 pub trait ToHexString {
     fn to_prefixed_hex_str(&self) -> String;
     fn to_nonprefixed_hex_str(&self) -> String;
-}
-
-pub trait HashIdentified {
-    type Element;
-    type Hash;
-
-    fn get_by_hash(&self, hash: Self::Hash) -> Self::Element;
-}
-
-pub trait Accounted {
-    fn deploy(&self, state: impl StateChanger) -> Result<(), Error>;
-    fn declare(&self, state: &mut impl StateChanger) -> Result<(), Error>;
-}
-
-pub trait StateChanger {
-    fn declare_contract_class(&mut self, hash: ClassHash, contract_class: &ContractClass) -> Result<(), Error>;
-}
-
-pub trait AccountGenerator {
-    type Acc: Accounted;
-    fn generate_accounts(
-        &self,
-        number_of_accounts: u8,
-        class_hash: ClassHash,
-        contract_class: ContractClass,
-    ) -> DevnetResult<Vec<Self::Acc>>;
 }
 
 pub trait HashProducer {
