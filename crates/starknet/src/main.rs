ce1b2504edbd9595a7c1dd1c4d20df53a4f2bcea
marioiordanov <mio@shardlabs.io>
2023-06-23 09:39:28 +0300

    configure logging

--- crates/starknet/src/main.rs
+++ crates/starknet/src/main.rs
@@ -1,19 +1,45 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::{
+    env,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+};
 
 use ::server::ServerConfig;
+use tracing::{info};
+use tracing_subscriber::{
+    EnvFilter,
+};
 
 mod api;
 mod server;
 
+/// Configures tracing with default level INFO,
+/// If the environment variable `RUST_LOG` is set, it will be used instead.
+fn configure_tracing() {
+    let level_filter_layer = EnvFilter::builder()
+        .with_default_directive(tracing::Level::INFO.into())
+        .from_env_lossy();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(level_filter_layer)
+        .init();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    tracing_subscriber::fmt::init();
+    configure_tracing();
+
+    let port = env::var("DEVNET_PORT")
+        .expect("DEVNET_PORT must be set")
+        .parse::<u16>()
+        .expect("DEVNET_PORT must be a valid port number");
+
     let host = IpAddr::V4(Ipv4Addr::LOCALHOST);
-    let port = 8077;
+
     let mut addr = SocketAddr::new(host, port);
     let server = server::serve_http_api_json_rpc(addr, ServerConfig::default());
     addr = server.local_addr();
-    println!("{:?}", addr);
+
+    info!("StarkNet Devnet listening on {}", addr);
 
     // spawn the server on a new task
     let serve = tokio::task::spawn(server);
