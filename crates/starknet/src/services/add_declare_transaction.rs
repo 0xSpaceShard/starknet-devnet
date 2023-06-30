7c6a83c0623661ec051d823ba09ba4b132fcf5ba
marioiordanov <mio@shardlabs.io>
2023-06-30 09:22:41 +0300

    add declare transcation implementation

--- crates/starknet/src/services/add_declare_transaction.rs
+++ crates/starknet/src/services/add_declare_transaction.rs
@@ -1,17 +1,26 @@
-use starknet_in_rust::transaction::Declare;
-use starknet_types::{DevnetResult, felt::{TransactionHash, ClassHash}, traits::HashProducer};
+use starknet_in_rust::{state::StateDiff, transaction::Declare};
+use starknet_types::{
+    felt::{ClassHash, TransactionHash},
+    traits::HashProducer,
+    DevnetResult,
+};
 
-use crate::{Starknet, transactions::{declare_transaction::DeclareTransactionV1, StarknetTransaction, Transaction}};
+use crate::{
+    transactions::{declare_transaction::DeclareTransactionV1, StarknetTransaction, Transaction},
+    Starknet,
+};
 
 impl Starknet {
-    pub(crate) fn add_declare_transaction(
+    pub fn add_declare_transaction(
         &mut self,
         declare_transaction: DeclareTransactionV1,
     ) -> DevnetResult<(TransactionHash, ClassHash)> {
+        let mut declare_transaction = declare_transaction;
         let class_hash = declare_transaction.contract_class.clone().generate_hash()?;
         let transaction_hash = declare_transaction.generate_hash()?;
+        declare_transaction.transaction_hash = Some(transaction_hash);
 
-        let declare_transaction_result = Declare {
+        let transaction = Declare {
             class_hash: class_hash.into(),
             sender_address: declare_transaction.sender_address.try_into()?,
             tx_type: starknet_in_rust::definitions::transaction_type::TransactionType::Declare,
@@ -24,24 +33,39 @@ impl Starknet {
             nonce: declare_transaction.nonce.into(),
             hash_value: transaction_hash.into(),
             contract_class: declare_transaction.contract_class.clone().try_into()?,
-        }
-        .execute(&mut self.state.pending_state, &self.block_context);
-
-        let transaction_to_add = match declare_transaction_result {
-            Ok(tx_info) => StarknetTransaction::create_pending(
-                Transaction::Declare(declare_transaction),
-                tx_info,
-            ),
-            Err(err) => {
-                StarknetTransaction::create_rejected(Transaction::Declare(declare_transaction), err)
-            }
         };
 
+        transaction.verify_version()?;
+
+        match transaction.execute(&mut self.state.pending_state, &self.block_context) {
+            Ok(tx_info) => {
+                let transaction_to_add = StarknetTransaction::create_accepted(
+                    Transaction::Declare(declare_transaction.clone()),
+                tx_info,
+                );
+
+                // add accepted transaction to pending block
+                self.blocks
+                    .pending_block
+                    .add_transaction(Transaction::Declare(declare_transaction));
+                // add transaction to transactions
+                self.transactions.insert(&transaction_hash, transaction_to_add);
+
+                self.generate_new_block()?;
+                self.create_pending_block()?;
+            }
+            Err(tx_err) => {
+                let transaction_to_add = StarknetTransaction::create_rejected(
+                    Transaction::Declare(declare_transaction.clone()),
+                    tx_err,
+                );
+
         self.transactions.insert(&transaction_hash, transaction_to_add);
+            }
+        }
 
         Ok((class_hash, class_hash))
     }
-
 }
 
 #[cfg(test)]
