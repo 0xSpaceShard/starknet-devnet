0c4f7faf0d15affdcebc32f2921b6bcd785d2cf9
marioiordanov <mio@shardlabs.io>
2023-07-05 21:12:07 +0300

    remove unused display trait implementation

--- crates/starknet-server/src/api/models/mod.rs
+++ crates/starknet-server/src/api/models/mod.rs
@@ -53,12 +53,6 @@ pub struct FeltHex(
     pub Felt,
 );
 
-impl Display for FeltHex {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(self.0.to_prefixed_hex_str().as_str())
-    }
-}
-
 /// Contract address serialized/deserialized from/to prefixed hex string
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct ContractAddressHex(
