de08e6f01fd9e813309bff9d72d020e393077bba
marioiordanov <mio@shardlabs.io>
2023-07-06 14:41:53 +0300

    format

--- crates/starknet-server/src/api/http/endpoints/accounts.rs
+++ crates/starknet-server/src/api/http/endpoints/accounts.rs
@@ -1,22 +1,27 @@
-use axum::{extract::Query, Json, Extension};
+use axum::{extract::Query, Extension, Json};
 use starknet_types::traits::ToHexString;
 
 use crate::api::http::{
     error::HttpApiError,
     models::{Balance, ContractAddress, PredeployedAccount},
-    HttpApiResult, HttpApiHandler,
+    HttpApiHandler, HttpApiResult,
 };
 
-pub(crate) async fn predeployed_accounts(Extension(state): Extension<HttpApiHandler>) -> HttpApiResult<Json<Vec<PredeployedAccount>>> {
-    let predeployed_accounts = state.api.starknet.read().await.get_predeployed_accounts()
+pub(crate) async fn predeployed_accounts(
+    Extension(state): Extension<HttpApiHandler>,
+) -> HttpApiResult<Json<Vec<PredeployedAccount>>> {
+    let predeployed_accounts = state
+        .api
+        .starknet
+        .read()
+        .await
+        .get_predeployed_accounts()
         .into_iter()
-        .map(|acc| {
-            PredeployedAccount{
-                initial_balance: 0,
-                address: acc.account_address.to_prefixed_hex_str(),
-                public_key: acc.public_key.to_prefixed_hex_str(),
-                private_key: acc.private_key.to_prefixed_hex_str(),
-            }
+        .map(|acc| PredeployedAccount {
+            initial_balance: 0,
+            address: acc.account_address.to_prefixed_hex_str(),
+            public_key: acc.public_key.to_prefixed_hex_str(),
+            private_key: acc.private_key.to_prefixed_hex_str(),
         })
         .collect::<Vec<PredeployedAccount>>();
 
