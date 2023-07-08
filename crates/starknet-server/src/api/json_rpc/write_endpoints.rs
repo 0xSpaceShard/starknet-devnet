b03582c3d4f24f23c31c43c55cc284b1c50db418
marioiordanov <mio@shardlabs.io>
2023-07-08 12:51:19 +0300

    format

--- crates/starknet-server/src/api/json_rpc/write_endpoints.rs
+++ crates/starknet-server/src/api/json_rpc/write_endpoints.rs
@@ -1,6 +1,8 @@
 use super::RpcResult;
 use crate::api::json_rpc::JsonRpcHandler;
-use crate::api::models::transaction::{BroadcastedDeclareTransaction, BroadcastedDeclareTransactionV1};
+use crate::api::models::transaction::{
+    BroadcastedDeclareTransaction, BroadcastedDeclareTransactionV1,
+};
 
 impl JsonRpcHandler {
     pub(crate) async fn add_declare_transaction(
@@ -8,9 +10,11 @@ impl JsonRpcHandler {
         request: BroadcastedDeclareTransaction,
     ) -> RpcResult<()> {
         match request {
-            BroadcastedDeclareTransaction::V1(BroadcastedDeclareTransactionV1{common, contract_class, sender_address}) => {
-                
-            },
+            BroadcastedDeclareTransaction::V1(BroadcastedDeclareTransactionV1 {
+                common,
+                contract_class,
+                sender_address,
+            }) => {}
             BroadcastedDeclareTransaction::V2(_) => todo!(),
         }
 
