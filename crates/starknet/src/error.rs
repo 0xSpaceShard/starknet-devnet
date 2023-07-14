40a3a1b51663676cb4c507bf88e0c5796f261481
edwin <ep@shardlabs.io>
2023-07-14 10:30:03 +0300

    Starknet errors added

---
+++ crates/starknet/src/error.rs
@@ -0,0 +1,26 @@
+use starknet_types;
+
+use starknet_in_rust;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    StarknetApiError(#[from] starknet_api::StarknetApiError),
+    #[error(transparent)]
+    StateError(#[from] starknet_in_rust::core::errors::state_errors::StateError),
+    #[error(transparent)]
+    TransactionError(#[from] starknet_in_rust::transaction::error::TransactionError),
+    #[error("Types error")]
+    TypesError(#[from] starknet_types::error::Error),
+    #[error("Specifying block by hash is currently not enabled")]
+    BlockIdHashUnimplementedError,
+    #[error("Specifying block by number is currently not enabled")]
+    BlockIdNumberUnimplementedError,
+    #[error("I/O error")]
+    IoError(#[from] std::io::Error),
+    #[error("Error when reading file {path}")]
+    ReadFileError { source: std::io::Error, path: String },
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
