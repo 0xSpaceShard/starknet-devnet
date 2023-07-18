2d835553f352b51c67981a92518374918b3d2c62
FabijanC <fabijan.corak@gmail.com>
2023-07-18 17:29:04 +0200

    Remove redundant scripts [skip ci]

--- scripts/get_block_number.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-curl localhost:5050/rpc -X POST -H "Content-Type: application/json" --data '{
-        "jsonrpc":"2.0",
-        "method":"starknet_blockNumber",
-        "id":1
-}'
