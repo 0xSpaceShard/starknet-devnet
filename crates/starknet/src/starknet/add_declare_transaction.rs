40afe198a44aa1e597baeab40db7a0b17c1bdb04
marioiordanov <mio@shardlabs.io>
2023-07-18 18:29:58 +0300

    removed comment

--- crates/starknet/src/starknet/add_declare_transaction.rs
+++ crates/starknet/src/starknet/add_declare_transaction.rs
@@ -20,7 +20,6 @@ pub fn add_declare_transaction_v2(
     let transaction_hash = declare_transaction.generate_hash()?;
     declare_transaction.transaction_hash = Some(transaction_hash);
 
-    // TODO compile the contract and check the hash
     let transaction = DeclareV2::new(
         &declare_transaction.sierra_contract_class.clone().try_into()?,
         None,
