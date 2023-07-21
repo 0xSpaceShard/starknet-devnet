cbf49836aaa00026741aaec9c3ddab69ad86e1a8
marioiordanov <mio@shardlabs.io>
2023-07-21 12:12:32 +0300

    state diff struct change

--- crates/starknet/src/state_diff.rs
+++ crates/starknet/src/state_diff.rs
@@ -1,12 +1,18 @@
 use std::collections::HashMap;
 
-use starknet_types::felt::ClassHash;
-use starknet_types::contract_class::ContractClass;
 use starknet_in_rust::state::StateDiff as StarknetInRustStateDiff;
+use starknet_types::contract_class::ContractClass;
+use starknet_types::felt::ClassHash;
 
+/// This struct is used to store the difference between state modifications
 pub struct StateDiff {
+    // data taken from starknet_in_rust
     pub(crate) inner: StarknetInRustStateDiff,
+    // class hash to compiled_class_hash difference, used when declaring contracts
+    // that are different from cairo 0
     pub(crate) class_hash_to_compiled_class_hash: HashMap<ClassHash, ClassHash>,
+    // declare contracts that are not cairo 0
+    pub(crate) declared_contracts: HashMap<ClassHash, ContractClass>,
+    // cairo 0 declared contracts
     pub(crate) cairo_0_declared_contracts: HashMap<ClassHash, ContractClass>,
-    pub(crate) cairo_1_declared_contracts: HashMap<ClassHash, ContractClass>,
 }
