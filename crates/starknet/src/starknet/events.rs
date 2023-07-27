728e4f9501a8e27cf4c44696aee7c61e7853687a
marioiordanov <mio@shardlabs.io>
2023-07-27 12:29:14 +0300

    progress

--- crates/starknet/src/starknet/events.rs
+++ crates/starknet/src/starknet/events.rs
@@ -1,6 +1,7 @@
 use starknet_rs_core::types::BlockId;
 use starknet_types::contract_address::ContractAddress;
 use starknet_types::felt::Felt;
+use crate::error::Result;
 
 use super::Starknet;
 
@@ -10,6 +11,23 @@ pub(crate) fn get_events(
     to_block: Option<BlockId>,
     address: ContractAddress,
     keys: Vec<Felt>,
-) {
-    // starknet.blocks.
+) -> Result<()>{
+    let blocks = starknet.blocks.get_blocks(from_block, to_block)?;
+    for block in blocks {
+        for transaction in block.get_transactions() {
+
+            // let events = transaction.get_events();
+            // for event in events {
+            //     if event.contract_address == address {
+            //         let event_data = event.get_data();
+            //         for key in keys {
+            //             if event_data.contains_key(&key) {
+            //                 println!("{}: {}", key, event_data.get(&key).unwrap());
+            //             }
+            //         }
+            //     }
+            // }
+        }
+    }
+    Ok(())
 }
