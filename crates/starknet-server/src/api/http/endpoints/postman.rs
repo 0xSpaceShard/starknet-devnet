e8b4f78d94bf32bf3af8184c73a34a09217928ea
edwin <ep@shardlabs.io>
2023-07-28 12:37:47 +0300

    Some compilation fixes

--- crates/starknet-server/src/api/http/endpoints/postman.rs
+++ crates/starknet-server/src/api/http/endpoints/postman.rs
@@ -5,7 +5,7 @@ use crate::api::http::models::{
     MessageFromL2, MessageHash, MessageToL2, PostmanLoadL1MessagingContract,
 };
 use crate::api::http::HttpApiResult;
-use crate::api::models::transaction::TransactionHashHex;
+use starknet_types::felt::TransactionHash;
 
 pub(crate) async fn postman_load(
     Json(_l1_contract): Json<PostmanLoadL1MessagingContract>,
@@ -19,7 +19,7 @@ pub(crate) async fn postman_flush() -> HttpApiResult<()> {
 
 pub(crate) async fn postman_send_message_to_l2(
     Json(_data): Json<MessageToL2>,
-) -> HttpApiResult<Json<TransactionHashHex>> {
+) -> HttpApiResult<Json<TransactionHash>> {
     Err(HttpApiError::GeneralError)
 }
 
