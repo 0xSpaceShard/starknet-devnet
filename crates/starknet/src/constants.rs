9795932d96e5df85b13acc40259ea158d5718038
FabijanC <fabijan.corak@gmail.com>
2023-10-23 15:37:20 +0200

    Rename account 0 artifact to casm

--- crates/starknet/src/constants.rs
+++ crates/starknet/src/constants.rs
@@ -4,7 +4,7 @@ use starknet_types::chain_id::ChainId;
 
 pub const CAIRO_0_ACCOUNT_CONTRACT_PATH: &str = concat!(
     env!("CARGO_MANIFEST_DIR"),
-    "/accounts_artifacts/OpenZeppelin/0.5.1/Account.cairo/Account.json"
+    "/accounts_artifacts/OpenZeppelin/0.5.1/Account.cairo/Account.casm"
 );
 pub const CAIRO_0_ACCOUNT_CONTRACT_HASH: &str =
     "0x4d07e40e93398ed3c76981e72dd1fd22557a78ce36c0515f679e27f0bb5bc5f";
