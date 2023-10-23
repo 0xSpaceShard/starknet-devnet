d1d4ad1e1ed63bbd265d12a74810335499d53c46
mw <mikiw.pp@gmail.com>
2023-10-23 13:43:43 +0200

    Remove unnecessary test

--- crates/starknet-server/tests/test_dump_and_load.rs
+++ crates/starknet-server/tests/test_dump_and_load.rs
@@ -301,19 +301,6 @@ mod dump_and_load_tests {
     }
 
     #[tokio::test]
-    async fn dump_endpoint_fail_with_wrong_file_name() {
-        let devnet_dump = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
-        let dump_body = Body::from(
-            json!({
-                "path": "///"
-            })
-            .to_string(),
-        );
-        let result = devnet_dump.post_json("/dump".into(), dump_body).await.unwrap();
-        assert_eq!(result.status(), 400);
-    }
-
-    #[tokio::test]
     async fn load_endpoint_fail_with_wrong_path() {
         let load_file_name = "load_file_name";
         let devnet_load = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
