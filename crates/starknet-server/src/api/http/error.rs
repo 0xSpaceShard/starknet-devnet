3783015e42e3ae93029ebc6b3cdd7305c67d90e6
mw <mikiw.pp@gmail.com>
2023-10-27 14:13:12 +0200

    Update error.rs

--- crates/starknet-server/src/api/http/error.rs
+++ crates/starknet-server/src/api/http/error.rs
@@ -29,27 +29,15 @@ pub enum HttpApiError {
 impl IntoResponse for HttpApiError {
     fn into_response(self) -> axum::response::Response {
         let (status, error_message) = match self {
-            HttpApiError::GeneralError => {
-                (StatusCode::INTERNAL_SERVER_ERROR, HttpApiError::GeneralError.to_string())
-            }
-            HttpApiError::FileNotFound => {
-                (StatusCode::BAD_REQUEST, HttpApiError::FileNotFound.to_string())
-            }
+            err @ HttpApiError::GeneralError => (StatusCode::BAD_REQUEST, err.to_string()),
+            err @ HttpApiError::FileNotFound => (StatusCode::BAD_REQUEST, err.to_string()),
             err @ HttpApiError::DumpError { msg: _ } => (StatusCode::BAD_REQUEST, err.to_string()),
-            HttpApiError::LoadError => {
-                (StatusCode::BAD_REQUEST, HttpApiError::LoadError.to_string())
-            }
-            HttpApiError::ReExecutionError => {
-                (StatusCode::BAD_REQUEST, HttpApiError::ReExecutionError.to_string())
-            }
-            HttpApiError::CreateEmptyBlockError => {
-                (StatusCode::BAD_REQUEST, HttpApiError::CreateEmptyBlockError.to_string())
-            }
-            HttpApiError::BlockSetTimeError => {
-                (StatusCode::BAD_REQUEST, HttpApiError::BlockSetTimeError.to_string())
-            }
-            HttpApiError::BlockIncreaseTimeError => {
-                (StatusCode::BAD_REQUEST, HttpApiError::BlockIncreaseTimeError.to_string())
+            err @ HttpApiError::LoadError => (StatusCode::BAD_REQUEST, err.to_string()),
+            err @ HttpApiError::ReExecutionError => (StatusCode::BAD_REQUEST, err.to_string()),
+            err @ HttpApiError::CreateEmptyBlockError => (StatusCode::BAD_REQUEST, err.to_string()),
+            err @ HttpApiError::BlockSetTimeError => (StatusCode::BAD_REQUEST, err.to_string()),
+            err @ HttpApiError::BlockIncreaseTimeError => {
+                (StatusCode::BAD_REQUEST, err.to_string())
             }
             err @ HttpApiError::MintingError { msg: _ } => {
                 (StatusCode::BAD_REQUEST, err.to_string())
