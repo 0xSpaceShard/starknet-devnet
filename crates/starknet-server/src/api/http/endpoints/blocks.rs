a30d99b167e41a95a475b1cfdd8af5f9d19f615d
Mikołaj Woźniak <mikiw.pp@gmail.com>
2023-11-03 16:42:15 +0100

    Advancing time endpoints (#217)

--- crates/starknet-server/src/api/http/endpoints/blocks.rs
+++ crates/starknet-server/src/api/http/endpoints/blocks.rs
@@ -1,11 +1,22 @@
-use axum::Json;
+use axum::{Extension, Json};
 
 use crate::api::http::error::HttpApiError;
 use crate::api::http::models::{AbortedBlocks, AbortingBlocks, CreatedBlock};
-use crate::api::http::HttpApiResult;
+use crate::api::http::{HttpApiHandler, HttpApiResult};
 
-pub(crate) async fn create_block() -> HttpApiResult<Json<CreatedBlock>> {
-    Err(HttpApiError::GeneralError)
+pub(crate) async fn create_block(
+    Extension(state): Extension<HttpApiHandler>,
+) -> HttpApiResult<Json<CreatedBlock>> {
+    let mut starknet = state.api.starknet.write().await;
+    starknet
+        .create_block(None)
+        .map_err(|err| HttpApiError::CreateEmptyBlockError { msg: err.to_string() })?;
+
+    let last_block = starknet.get_latest_block();
+    match last_block {
+        Ok(block) => Ok(Json(CreatedBlock { block_hash: block.block_hash() })),
+        Err(err) => Err(HttpApiError::CreateEmptyBlockError { msg: err.to_string() }),
+    }
 }
 
 pub(crate) async fn abort_blocks(
