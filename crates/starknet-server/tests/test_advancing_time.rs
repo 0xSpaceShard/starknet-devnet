765e799cb6f5b429c51108c0237492238cdcaaa1
mw <mikiw.pp@gmail.com>
2023-11-09 15:06:10 +0200

    Add BUFFER_TIME_SECONDS in tests

--- crates/starknet-server/tests/test_advancing_time.rs
+++ crates/starknet-server/tests/test_advancing_time.rs
@@ -10,12 +10,14 @@ mod advancing_time_tests {
     use crate::common::devnet::BackgroundDevnet;
     use crate::common::utils::{get_json_body, get_unix_timestamp_as_seconds};
 
-    static DUMMY_ADDRESS: u128 = 1;
-    static DUMMY_AMOUNT: u128 = 1;
+    const DUMMY_ADDRESS: u128 = 1;
+    const DUMMY_AMOUNT: u128 = 1;
+    const BUFFER_TIME_SECONDS: u64 = 5;
 
     #[tokio::test]
     async fn set_time_in_past() {
-        // set time and assert
+        // set time and assert if it's greater/equal than past_time, check if it's inside buffer
+        // limit
         let past_time = 1;
         let devnet = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
         let set_time_body = Body::from(
@@ -31,31 +33,37 @@ mod advancing_time_tests {
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(set_time_block["timestamp"].as_u64() >= Some(past_time));
+        assert!(set_time_block["timestamp"].as_u64() < Some(past_time + BUFFER_TIME_SECONDS));
 
         // wait 1 second
         thread::sleep(time::Duration::from_secs(1));
 
-        // create block and check if block_timestamp is greater than past_time
+        // create block and check if block_timestamp is greater than past_time, check if it's inside
+        // buffer limit
         devnet.post_json("/create_block".into(), Body::from(json!({}).to_string())).await.unwrap();
         let empty_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(empty_block["timestamp"].as_u64() > Some(past_time));
+        assert!(empty_block["timestamp"].as_u64() < Some(past_time + BUFFER_TIME_SECONDS));
 
         // wait 1 second
         thread::sleep(time::Duration::from_secs(1));
 
-        // check if after mint timestamp is greater than last block
+        // check if after mint timestamp is greater than last block, check if it's inside buffer
+        // limit
         devnet.mint(DUMMY_ADDRESS, DUMMY_AMOUNT).await;
         let mint_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(mint_block["timestamp"].as_u64() > empty_block["block_timestamp"].as_u64());
+        assert!(mint_block["timestamp"].as_u64() < Some(past_time + BUFFER_TIME_SECONDS));
     }
 
     #[tokio::test]
     async fn set_time_in_future() {
-        // set time and assert
+        // set time and assert if it's greater/equal than future_time, check if it's inside buffer
+        // limit
         let now = get_unix_timestamp_as_seconds();
         let future_time = now + 100;
         let devnet = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
@@ -72,26 +80,31 @@ mod advancing_time_tests {
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(set_time_block["timestamp"].as_u64() >= Some(future_time));
+        assert!(set_time_block["timestamp"].as_u64() < Some(future_time + BUFFER_TIME_SECONDS));
 
         // wait 1 second
         thread::sleep(time::Duration::from_secs(1));
 
-        // create block and check if block_timestamp is greater than future_time
+        // create block and check if block_timestamp is greater than future_time, check if it's
+        // inside buffer limit
         devnet.post_json("/create_block".into(), Body::from(json!({}).to_string())).await.unwrap();
         let empty_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(empty_block["timestamp"].as_u64() > Some(future_time));
+        assert!(empty_block["timestamp"].as_u64() < Some(future_time + BUFFER_TIME_SECONDS));
 
         // wait 1 second
         thread::sleep(time::Duration::from_secs(1));
 
-        // check if after mint timestamp is greater than last block
+        // check if after mint timestamp is greater than last empty block, check if it's inside
+        // buffer limit
         devnet.mint(DUMMY_ADDRESS, DUMMY_AMOUNT).await;
         let mint_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(mint_block["timestamp"].as_u64() > empty_block["timestamp"].as_u64());
+        assert!(mint_block["timestamp"].as_u64() < Some(future_time + BUFFER_TIME_SECONDS));
     }
 
     #[tokio::test]
@@ -120,7 +133,7 @@ mod advancing_time_tests {
         let devnet = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
         let now = get_unix_timestamp_as_seconds();
 
-        // increase time and assert if it's greater than now
+        // increase time and assert if it's greater than now, check if it's inside buffer limit
         let first_increase_time: u64 = 1000;
         let first_increase_time_body = Body::from(
             json!({
@@ -133,8 +146,12 @@ mod advancing_time_tests {
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(first_increase_time_block["timestamp"].as_u64() >= Some(now + first_increase_time));
+        assert!(
+            first_increase_time_block["timestamp"].as_u64()
+                < Some(now + first_increase_time + BUFFER_TIME_SECONDS)
+        );
 
-        // second increase time
+        // second increase time, check if it's inside buffer limit
         let second_increase_time: u64 = 100;
         let second_increase_time_body = Body::from(
             json!({
@@ -150,11 +167,16 @@ mod advancing_time_tests {
             second_increase_time_block["timestamp"].as_u64()
                 >= Some(now + first_increase_time + second_increase_time)
         );
+        assert!(
+            second_increase_time_block["timestamp"].as_u64()
+                < Some(now + first_increase_time + second_increase_time + BUFFER_TIME_SECONDS)
+        );
 
         // wait 1 second
         thread::sleep(time::Duration::from_secs(1));
 
-        // create block and check again if block_timestamp is greater than last block
+        // create block and check again if block_timestamp is greater than last block, check if it's
+        // inside buffer limit
         devnet.post_json("/create_block".into(), Body::from(json!({}).to_string())).await.unwrap();
         let empty_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
@@ -162,16 +184,25 @@ mod advancing_time_tests {
         assert!(
             empty_block["timestamp"].as_u64() > second_increase_time_block["timestamp"].as_u64()
         );
+        assert!(
+            empty_block["timestamp"].as_u64()
+                < Some(now + first_increase_time + second_increase_time + BUFFER_TIME_SECONDS)
+        );
 
         // wait 1 second
         thread::sleep(time::Duration::from_secs(1));
 
-        // check if after mint timestamp is greater than last block
+        // check if after mint timestamp is greater than last block, check if it's inside buffer
+        // limit
         devnet.mint(DUMMY_ADDRESS, DUMMY_AMOUNT).await;
         let last_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(last_block["timestamp"].as_u64() > empty_block["timestamp"].as_u64());
+        assert!(
+            last_block["timestamp"].as_u64()
+                < Some(now + first_increase_time + second_increase_time + BUFFER_TIME_SECONDS)
+        );
     }
 
     #[tokio::test]
@@ -203,7 +234,6 @@ mod advancing_time_tests {
 
     #[tokio::test]
     async fn start_time_in_past() {
-        let now = get_unix_timestamp_as_seconds();
         let past_time = 1;
         let devnet = BackgroundDevnet::spawn_with_additional_args(&[
             "--start-time",
@@ -212,13 +242,14 @@ mod advancing_time_tests {
         .await
         .expect("Could not start Devnet");
 
-        // create block and check if block timestamp is greater/equal 1 and less than now
+        // create block and check if block timestamp is greater/equal 1, check if it's inside buffer
+        // limit
         devnet.post_json("/create_block".into(), Body::from(json!({}).to_string())).await.unwrap();
         let empty_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(empty_block["timestamp"].as_u64() >= Some(past_time));
-        assert!(empty_block["timestamp"].as_u64() < Some(now));
+        assert!(empty_block["timestamp"].as_u64() < Some(past_time + BUFFER_TIME_SECONDS));
     }
 
     #[tokio::test]
@@ -232,12 +263,14 @@ mod advancing_time_tests {
         .await
         .expect("Could not start Devnet");
 
-        // create block and check if block timestamp is greater than now
+        // create block and check if block timestamp is greater than now, check if it's inside
+        // buffer limit
         devnet.post_json("/create_block".into(), Body::from(json!({}).to_string())).await.unwrap();
         let empty_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(empty_block["timestamp"].as_u64() > Some(now));
+        assert!(empty_block["timestamp"].as_u64() < Some(future_time + BUFFER_TIME_SECONDS));
     }
 
     #[tokio::test]
@@ -252,7 +285,7 @@ mod advancing_time_tests {
         .expect("Could not start Devnet");
 
         // increase time and assert if it's greater/equal than start-time argument +
-        // first_increase_time
+        // first_increase_time, check if it's inside buffer limit
         let first_increase_time: u64 = 1000;
         let first_increase_time_body = Body::from(
             json!({
@@ -268,9 +301,13 @@ mod advancing_time_tests {
             first_increase_time_block["timestamp"].as_u64()
                 >= Some(past_time + first_increase_time)
         );
+        assert!(
+            first_increase_time_block["timestamp"].as_u64()
+                < Some(past_time + first_increase_time + BUFFER_TIME_SECONDS)
+        );
 
         // increase the time a second time and assert if it's greater/equal than past_time +
-        // first_increase_time + second_increase_time
+        // first_increase_time + second_increase_time, check if it's inside buffer limit
         let second_increase_time: u64 = 100;
         let second_increase_time_body = Body::from(
             json!({
@@ -286,8 +323,15 @@ mod advancing_time_tests {
             second_increase_time_block["timestamp"].as_u64()
                 >= Some(past_time + first_increase_time + second_increase_time)
         );
+        assert!(
+            second_increase_time_block["timestamp"].as_u64()
+                < Some(
+                    past_time + first_increase_time + second_increase_time + BUFFER_TIME_SECONDS
+                )
+        );
 
-        // set time to be now and check if the latest block timestamp is greater/equal now
+        // set time to be now and check if the latest block timestamp is greater/equal now, check if
+        // it's inside buffer limit
         let set_time_body = Body::from(
             json!({
                 "time": now
@@ -301,20 +345,22 @@ mod advancing_time_tests {
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(set_time_block["timestamp"].as_u64() >= Some(now));
+        assert!(set_time_block["timestamp"].as_u64() < Some(now + BUFFER_TIME_SECONDS));
 
         // wait 1 second
         thread::sleep(time::Duration::from_secs(1));
 
         // create a new empty block and check again if block timestamp is greater than
-        // set_time_block
+        // set_time_block, check if it's inside buffer limit
         devnet.post_json("/create_block".into(), Body::from(json!({}).to_string())).await.unwrap();
         let empty_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(empty_block["timestamp"].as_u64() > set_time_block["timestamp"].as_u64());
+        assert!(empty_block["timestamp"].as_u64() < Some(now + BUFFER_TIME_SECONDS));
 
         // increase the time a third time and assert if it's greater/equal than last empty block
-        // timestamp + third_increase_time
+        // timestamp + third_increase_time, check if it's inside buffer limit
         let third_increase_time: u64 = 10000;
         let third_increase_time_body = Body::from(
             json!({
@@ -330,15 +376,24 @@ mod advancing_time_tests {
             third_increase_time_block["timestamp"].as_u64().unwrap()
                 >= empty_block["timestamp"].as_u64().unwrap() + third_increase_time
         );
+        assert!(
+            third_increase_time_block["timestamp"].as_u64()
+                < Some(now + third_increase_time + BUFFER_TIME_SECONDS)
+        );
 
         // wait 1 second
         thread::sleep(time::Duration::from_secs(1));
 
-        // check if the last block timestamp is greater previous block
+        // check if the last block timestamp is greater previous block, check if it's inside buffer
+        // limit
         devnet.post_json("/create_block".into(), Body::from(json!({}).to_string())).await.unwrap();
         let last_block = &devnet
             .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
             .await["result"];
         assert!(last_block["timestamp"].as_u64() > third_increase_time_block["timestamp"].as_u64());
+        assert!(
+            last_block["timestamp"].as_u64()
+                < Some(now + third_increase_time + BUFFER_TIME_SECONDS)
+        );
     }
 }
