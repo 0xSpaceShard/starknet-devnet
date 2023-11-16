006a70d6e4faf99b0a326e687a67029bb93fbba9
marioiordanov <mio@shardlabs.io>
2023-11-16 16:32:48 +0200

    not needed serde attribute

--- crates/starknet-server/src/api/json_rpc/spec_reader/spec_schemas/mod.rs
+++ crates/starknet-server/src/api/json_rpc/spec_reader/spec_schemas/mod.rs
@@ -26,7 +26,6 @@ pub struct Common {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "additionalProperties")]
     pub additional_properties: Option<bool>,
     #[serde(rename = "type")]
     pub t: Option<String>,
