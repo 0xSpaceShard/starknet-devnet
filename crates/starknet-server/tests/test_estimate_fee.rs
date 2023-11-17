49f85392120f8ea0c634b431b14c39ae42cb61f9
marioiordanov <mio@shardlabs.io>
2023-11-17 14:16:40 +0200

    fmt

--- crates/starknet-server/tests/test_estimate_fee.rs
+++ crates/starknet-server/tests/test_estimate_fee.rs
@@ -9,7 +9,8 @@ mod estimate_fee_tests {
     };
     use starknet_core::utils::exported_test_utils::dummy_cairo_0_contract_class;
     use starknet_rs_accounts::{
-        Account, AccountError, AccountFactory, AccountFactoryError, Call, ConnectedAccount, ExecutionEncoding, OpenZeppelinAccountFactory, SingleOwnerAccount,
+        Account, AccountError, AccountFactory, AccountFactoryError, Call, ConnectedAccount,
+        ExecutionEncoding, OpenZeppelinAccountFactory, SingleOwnerAccount,
     };
     use starknet_rs_contract::ContractFactory;
     use starknet_rs_core::types::contract::legacy::LegacyContractClass;
