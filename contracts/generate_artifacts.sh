bce47a80acfc5b7b473aba1b03e097181e82f41b
glihm <dev@glihm.net>
2023-11-26 20:45:34 -0600

    fix: correct the artifacts generation

--- contracts/generate_artifacts.sh
+++ contracts/generate_artifacts.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Bash script to generate the artifacts required by Devnet to compile.
+mkdir -p artifacts
 
+# L1-L2 messaging:
 ## SOLIDITY
-forge build --root ./solidity
-cp ./solidity/out/MockStarknetMessaging.sol/MockStarknetMessaging.json ./artifacts/MockStarknetMessaging.json
+forge build --root ./l1-l2-messaging/solidity
+cp ./l1-l2-messaging/solidity/out/MockStarknetMessaging.sol/MockStarknetMessaging.json ./artifacts/
 
 ## CAIRO
+scarb --manifest-path ./l1-l2-messaging/cairo/Scarb.toml build
+cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2.contract_class.json ./artifacts/
