efa00f118a1078f56c705f5348b8c895e3cbfc9d
marioiordanov <mio@shardlabs.io>
2023-12-01 14:44:20 +0200

    added new constant for starknet fee token address

--- crates/starknet/src/constants.rs
+++ crates/starknet/src/constants.rs
@@ -26,6 +26,9 @@ pub const ERC20_CONTRACT_CLASS_HASH: &str =
 pub const ERC20_CONTRACT_ADDRESS: &str =
     "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7";
 
+pub const STRK_FEE_CONTRACT_ADDRESS: &str =
+    "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
+
 pub(crate) const UDC_CONTRACT_PATH: &str =
     concat!(env!("CARGO_MANIFEST_DIR"), "/accounts_artifacts/UDC_OZ_0.5.0.json");
 
