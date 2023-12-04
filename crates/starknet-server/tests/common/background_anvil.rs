e5719da848fea7055694f3a04c259dd8c403d9b9
FabijanC <fabijan.corak@gmail.com>
2023-12-04 11:46:00 +0100

    Refactor spawning of background anvil

--- crates/starknet-server/tests/common/background_anvil.rs
+++ crates/starknet-server/tests/common/background_anvil.rs
@@ -28,6 +28,9 @@ impl BackgroundAnvil {
     /// Anvil on this port (as Anvil will actually open the socket right after binding).
     #[allow(dead_code)] // dead_code needed to pass clippy
     pub(crate) async fn spawn() -> Result<Self, TestError> {
+        // Relies on `background_devnet::BackgroundDevnet` starting its check from smaller values
+        // (1025). Relies on the probability of M simultaneously spawned Anvils occupying
+        // different ports being fairly big (N*(N-1)*...*(N-M+1) / N**M; N=65_000-20_000+1)
         let port = rand::thread_rng().gen_range(20_000..=65_000);
 
         let process = Command::new("anvil")
@@ -43,10 +46,10 @@ impl BackgroundAnvil {
         let max_retries = 10;
 
         while retries < max_retries {
-            if let Ok(anvil_block_rsp) = get_block_number(&anvil_url).await {
+            if let Ok(anvil_block_rsp) = send_dummy_request(&anvil_url).await {
                 assert_eq!(anvil_block_rsp.status(), StatusCode::OK);
                 println!("Spawned background anvil at port {port} (127.0.0.1)");
-                return Ok(BackgroundAnvil { process, url: anvil_url });
+                return Ok(Self { process, url: anvil_url });
             }
 
             retries += 1;
@@ -57,24 +60,23 @@ impl BackgroundAnvil {
     }
 }
 
-pub async fn get_block_number(rpc_url: &str) -> Result<Response<hyper::Body>, hyper::Error> {
-    let req = request::Request::builder()
-        .method("POST")
-        .uri(rpc_url)
+/// Even if the RPC method is dummy (doesn't exist),
+/// the server is expected to respond properly if alive
+async fn send_dummy_request(rpc_url: &str) -> Result<Response<hyper::Body>, hyper::Error> {
+    let req = request::Request::post(rpc_url)
         .header("content-type", "application/json")
-        .body(
-            r#"{
-    "jsonrpc": "2.0",
-    "method": "eth_blockNumberfuiorhgorueh",
-    "params": []
-    "id": "1"
-}"#
-            .into(),
-        )
+        .body(hyper::Body::from(
+            serde_json::json!({
+                "jsonrpc": "2.0",
+                "method": "eth_blockNumberfuiorhgorueh",
+                "params": [],
+                "id": "1"
+            })
+            .to_string(),
+        ))
         .unwrap();
 
-    let http_client = Client::new();
-    http_client.request(req).await
+    Client::new().request(req).await
 }
 
 /// By implementing Drop, we ensure there are no zombie background Anvil processes
