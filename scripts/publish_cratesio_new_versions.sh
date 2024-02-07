7186b9269a516208b8cdaf2a70ec0468d9b5d270
FabijanC <fc@spaceshard.io>
2024-02-07 12:35:02 +0100

    Refactor crate publishing

--- scripts/publish_cratesio_new_versions.sh
+++ scripts/publish_cratesio_new_versions.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-set -eu
+set -euo pipefail
 
 cargo install cargo-get
-workspace_members=$(cargo get --delimiter ";" workspace.members)
 
-cargo login $CRATES_IO_API_KEY
+for workspace_member in $(cargo get --delimiter " " workspace.members); do
+    package_name=$(cargo get --entry "$workspace_member" package.name)
+    package_version=$(cargo get --entry "$workspace_member" package.version)
+    cratesio_version=$(cargo search "$package_name" | sed -n 's/'$package_name' = "\([^"]*\)".*/\1/p')
 
-IFS=';' read -a array <<< "$workspace_members"
+    # if local version is different from crates.io version, then publish to crates.io
+    if [ "$package_version" != "$cratesio_version" ]; then
+        echo "Local version of $package_name is $package_version, while the one on crates.io is $cratesio_version"
 
-for workspace_member in "${array[@]}"
-do
-    crate_dir=$workspace_member
-
-    package_name=$(cargo get --entry $crate_dir package.name)
-    package_version=$(cargo get --entry $crate_dir package.version)
-    cratesio_version=$(cargo search $package_name | sed -n 's/'$package_name' = "\([^"]*\)".*/\1/p');
-
-    # if local version is different from crates.io version, then publish
-    if [ $package_version != $cratesio_version ]; then
-        echo "Local version of $package_name is $package_version, while the one on crates.io is $cratesio_version";
-        # Publish to crates.io
-        cargo publish -p $package_name
+        cargo login "$CRATES_IO_API_KEY"
+        cargo publish -p "$package_name"
+    else
+        echo "$package_name v$package_version already published"
     fi
-
 done
