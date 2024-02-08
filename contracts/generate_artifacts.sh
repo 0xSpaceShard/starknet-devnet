f482e0f54ef63a8406d35ea3ca65f2418575e982
FabijanC <fc@spaceshard.io>
2024-02-08 17:43:34 +0100

    Final changes related to crate renaming [skip ci] (#341)

--- contracts/generate_artifacts.sh
+++ contracts/generate_artifacts.sh
@@ -15,5 +15,5 @@ scarb --manifest-path ./l1-l2-messaging/cairo/Scarb.toml build
 cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2.contract_class.json ./artifacts/
 cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2_lib.contract_class.json ./artifacts/
 
-cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2.contract_class.json ../crates/starknet-devnet-cli/test_data/cairo1/messaging/cairo_1_l1l2.sierra
-cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2_lib.contract_class.json ../crates/starknet-devnet-cli/test_data/cairo1/messaging/cairo_1_l1l2_lib.sierra
+cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2.contract_class.json ../crates/starknet-devnet/test_data/cairo1/messaging/cairo_1_l1l2.sierra
+cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2_lib.contract_class.json ../crates/starknet-devnet/test_data/cairo1/messaging/cairo_1_l1l2_lib.sierra
