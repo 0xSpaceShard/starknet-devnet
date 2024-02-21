ec3fbdd3455237ba4a6a8e3f897ae4edc32a4507
mw <mw@spaceshard.io>
2024-02-21 14:59:19 +0100

    comment of keys

--- crates/starknet-devnet-core/src/constants.rs
+++ crates/starknet-devnet-core/src/constants.rs
@@ -23,7 +23,7 @@ pub const CAIRO_1_ERC20_CONTRACT_PATH: &str =
     concat!(env!("CARGO_MANIFEST_DIR"), "/accounts_artifacts/ERC20_Mintable_OZ_0.8.1.json");
 
 pub const CAIRO_1_ERC20_CONTRACT_CLASS_HASH: &str =
-    "0x02ce20303e4b963bed8f11fdb309f859822ee17be99399a9acdc289443bc9d09";
+    "0x046ded64ae2dead6448e247234bab192a9c483644395b66f2155f2614e5804b0";
 
 pub const CAIRO_0_ERC20_CONTRACT_PATH: &str =
     concat!(env!("CARGO_MANIFEST_DIR"), "/accounts_artifacts/ERC20_Mintable_OZ_0.2.0.json");
