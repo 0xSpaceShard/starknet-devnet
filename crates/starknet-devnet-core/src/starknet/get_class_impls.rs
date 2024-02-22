e3c69015fa19f261487ea094caee2c72cdc3a64b
FabijanC <fc@spaceshard.io>
2024-02-22 14:04:40 +0100

    Incorporate pending block generation into new block generation (#359)

--- crates/starknet-devnet-core/src/starknet/get_class_impls.rs
+++ crates/starknet-devnet-core/src/starknet/get_class_impls.rs
@@ -126,7 +126,6 @@ mod tests {
         let (mut starknet, account) = setup(Some(100000000), StateArchiveCapacity::Full);
 
         starknet.generate_new_block(StateDiff::default(), None).unwrap();
-        starknet.generate_pending_block().unwrap();
 
         let block_number = starknet.get_latest_block().unwrap().block_number();
         let block_id = BlockId::Number(block_number.0);
@@ -141,7 +140,6 @@ mod tests {
         let (mut starknet, account) = setup(Some(100000000), StateArchiveCapacity::None);
 
         starknet.generate_new_block(StateDiff::default(), None).unwrap();
-        starknet.generate_pending_block().unwrap();
 
         let block_number = starknet.get_latest_block().unwrap().block_number();
         let block_id = BlockId::Number(block_number.0);
@@ -158,7 +156,6 @@ mod tests {
         let (mut starknet, account) = setup(Some(100000000), StateArchiveCapacity::Full);
 
         starknet.generate_new_block(StateDiff::default(), None).unwrap();
-        starknet.generate_pending_block().unwrap();
 
         let block_number = starknet.get_latest_block().unwrap().block_number();
         let block_id = BlockId::Number(block_number.0);
