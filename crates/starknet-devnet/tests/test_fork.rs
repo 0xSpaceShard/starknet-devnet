c9d666d097d713a1b194cb56f5555874fa1e7852
mw <mw@spaceshard.io>
2024-03-01 16:23:49 +0000

    Add of test_forking_local_genesis_block
    [skip ci]

--- crates/starknet-devnet/tests/test_fork.rs
+++ crates/starknet-devnet/tests/test_fork.rs
@@ -1,41 +1,41 @@
 pub mod common;
 
 mod fork_tests {
-    use starknet_rs_core::types::{BlockId, BlockTag, FieldElement, StarknetError};
-    use starknet_rs_providers::{Provider, ProviderError};
+    use serde_json::json;
+    use starknet_rs_core::types::FieldElement;
 
     use crate::common::background_devnet::BackgroundDevnet;
-    use crate::common::constants::PREDEPLOYED_ACCOUNT_ADDRESS;
-
-    const ORIGIN_URL: &str = "https://alpha4.starknet.io";
-    const ALPHA_GOERLI_PREDEPLOYED_ACCOUNT_ADDRESS: &str = "0x0";
-    const ALPHA_GOERLI_EXPECTED_BALANCE: &str = "0x0";
-    const ALPHA_GOERLI_GENESIS_BLOCK: &str = "0x7d328a71faf48c5c3857e99f20a77b18522480956d1cd5bff1ff2df3c8b427b";
+    
+    static DUMMY_ADDRESS: u128 = 1;
+    static DUMMY_AMOUNT: u128 = 1;
+    const SEPOLIA_URL: &str = "https://alpha-sepolia.starknet.io";
+    const SEPOLIA_ACCOUNT_ADDRESS: &str = "0x0";
+    const SEPOLIA_EXPECTED_BALANCE: &str = "0x0";
+    const SEPOLIA_GENESIS_BLOCK: &str = "0x5c627d4aeb51280058bed93c7889bce78114d63baad1be0f0aeb32496d5f19c";
 
     #[tokio::test]
-    async fn test_forking_genesis_block() {
+    async fn test_forking_sepolia_genesis_block() {
         let fork_devnet = BackgroundDevnet::spawn_with_additional_args(&[
             "--fork-network",
-            ORIGIN_URL,
+            SEPOLIA_URL,
             "--accounts",
             "0",
         ])
         .await
         .expect("Could not start Devnet");
         
-        let genesis_block = &devnet
-            .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": ALPHA_GOERLI_GENESIS_BLOCK }))
+        let fork_genesis_block = &fork_devnet
+            .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": SEPOLIA_GENESIS_BLOCK }))
             .await["result"];
 
-        // TODO: add some asserts like block number = 0
-        println!("genesis_block {:?}", genesis_block);
+        assert_eq!(fork_genesis_block["block_number"], 0);
     }
 
     #[tokio::test]
-    async fn test_forking_contract_call_get_balance() {
+    async fn test_forking_sepolia_contract_call_get_balance() {
         let fork_devnet = BackgroundDevnet::spawn_with_additional_args(&[
             "--fork-network",
-            ORIGIN_URL,
+            SEPOLIA_URL,
             "--accounts",
             "0",
         ])
@@ -43,10 +43,40 @@ mod fork_tests {
         .expect("Could not start Devnet");
 
         // TODO: This should fail or get balance = 0? Maybe call for something different?
-        let contract_address = FieldElement::from_hex_be(ALPHA_GOERLI_PREDEPLOYED_ACCOUNT_ADDRESS).unwrap();
-        let retrieved_result = devnet.get_balance(&contract_address).await.unwrap();
+        let contract_address = FieldElement::from_hex_be(SEPOLIA_ACCOUNT_ADDRESS).unwrap();
+        let retrieved_result = fork_devnet.get_balance(&contract_address).await.unwrap();
 
-        let expected_balance = FieldElement::from_hex_be(ALPHA_GOERLI_EXPECTED_BALANCE.as_str()).unwrap();
+        let expected_balance = FieldElement::from_hex_be(SEPOLIA_EXPECTED_BALANCE).unwrap();
+        assert_eq!(retrieved_result, expected_balance);
+    }
+
+    #[tokio::test]
+    async fn test_forking_local_genesis_block() {
+        let devnet: BackgroundDevnet = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
+        // devnet.post_json("/create_block".into(), Body::from(json!({}).to_string())).await.unwrap();
+        devnet.mint(DUMMY_ADDRESS, DUMMY_AMOUNT).await;
+        let latest_block = &devnet
+            .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": "latest" }))
+            .await["result"];
+
+        let fork_devnet = BackgroundDevnet::spawn_with_additional_args(&[
+            "--fork-network",
+            devnet.url.as_str(),
+            // "--port", - is this needed?
+            // DEVNET_DEFAULT_PORT,
+            "--accounts",
+            "0",
+        ])
+        .await
+        .expect("Could not start Devnet");
+    
+        let fork_genesis_block = &fork_devnet
+            .send_custom_rpc("starknet_getBlockWithTxHashes", json!({ "block_id": latest_block["block_hash"] }))
+            .await["result"];
+        assert_eq!(fork_genesis_block["block_number"], 0);
+
+        let retrieved_result = fork_devnet.get_balance(&FieldElement::from(DUMMY_ADDRESS)).await.unwrap();
+        let expected_balance = FieldElement::from(DUMMY_AMOUNT);
         assert_eq!(retrieved_result, expected_balance);
     }
 }
