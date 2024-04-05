0938aed0b8b5a3dcaa3236db100da5155b50b7da
marioiordanov <mio@shardlabs.io>
2024-04-05 10:01:25 +0300

    copied QUERY_VERSION_OFFSET constant from starknet-rs

--- crates/starknet-devnet-types/src/constants.rs
+++ crates/starknet-devnet-types/src/constants.rs
@@ -1,3 +1,5 @@
+use starknet_rs_ff::FieldElement;
+
 pub const OUTPUT_BUILTIN_NAME: &str = "output_builtin";
 pub const HASH_BUILTIN_NAME: &str = "pedersen_builtin";
 pub const RANGE_CHECK_BUILTIN_NAME: &str = "range_check_builtin";
@@ -8,3 +10,11 @@ pub const KECCAK_BUILTIN_NAME: &str = "keccak_builtin";
 pub const POSEIDON_BUILTIN_NAME: &str = "poseidon_builtin";
 pub const SEGMENT_ARENA_BUILTIN_NAME: &str = "segment_arena_builtin";
 pub const N_STEPS: &str = "n_steps";
+
+// copied from starknet-rs, because it is not exposed as public type
+pub const QUERY_VERSION_OFFSET: FieldElement = FieldElement::from_mont([
+    18446744073700081665,
+    17407,
+    18446744073709551584,
+    576460752142434320,
+]);
