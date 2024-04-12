9edc21404f7a355f3dbe6f6d1d3ef748c0549e7b
FabijanC <fc@spaceshard.io>
2024-04-12 11:28:09 +0200

    Improve check_crate_changes.sh message [skip ci]

--- scripts/check_crate_changes.sh
+++ scripts/check_crate_changes.sh
@@ -13,4 +13,4 @@ echo "The crates that need a semver increment since git revision '$version' are:
 
 git diff "$version" --stat | grep -o -E 'crates/[^/]*' | uniq
 
-echo "Note that this does not reflect changes in Cargo.toml or changes that one Devnet crate may have had on another"
+echo "Note that this does not reflect dependency changes in Cargo.toml or changes that one Devnet crate may have had on another!"
