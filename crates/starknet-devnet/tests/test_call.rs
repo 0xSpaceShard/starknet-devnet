29bb2639baffc9bbdb931be2b89a7762aa2812ea
vibenedict <victorbenedict43@outlook.com>
2024-04-29 07:25:50 +0100

    test: old state support

--- crates/starknet-devnet/tests/test_call.rs
+++ crates/starknet-devnet/tests/test_call.rs
@@ -11,7 +11,7 @@ mod call {
     #[tokio::test]
     /// This test doesn't rely on devnet.get_balance because it's not supposed to call ERC20
     async fn calling_method_of_undeployed_contract() {
-        let devnet = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
+        let devnet = BackgroundDevnet::spawn_with_additional_args(&["--state-archive-capacity", "full"]).await.expect("Could not start Devnet");
         let contract_address = FieldElement::from_hex_be(PREDEPLOYED_ACCOUNT_ADDRESS).unwrap();
         let entry_point_selector =
             starknet_rs_core::utils::get_selector_from_name("balanceOf").unwrap();
@@ -34,11 +34,52 @@ mod call {
             ProviderError::StarknetError(StarknetError::ContractNotFound) => (),
             _ => panic!("Invalid error: {err:?}"),
         }
+
+        // Create new block
+        let block_hash = devnet.create_block().await;
+
+        // Test with newly created block's hash
+        let err = devnet
+            .json_rpc_client
+            .call(
+                FunctionCall {
+                    contract_address: undeployed_address,
+                    entry_point_selector,
+                    calldata: vec![contract_address],
+                },
+                BlockId::Hash(block_hash.unwrap()),
+            )
+            .await
+            .expect_err("Should have failed");
+
+        match err {
+            ProviderError::StarknetError(StarknetError::ContractNotFound) => (),
+            _ => panic!("Invalid error: {err:?}"),
+        }
+
+        // Test with block number
+        let err = devnet
+            .json_rpc_client
+            .call(
+                FunctionCall {
+                    contract_address: undeployed_address,
+                    entry_point_selector,
+                    calldata: vec![contract_address],
+                },
+                BlockId::Number(1),
+            )
+            .await
+            .expect_err("Should have failed");
+
+        match err {
+            ProviderError::StarknetError(StarknetError::ContractNotFound) => (),
+            _ => panic!("Invalid error: {err:?}"),
+        }
     }
 
     #[tokio::test]
     async fn calling_nonexistent_contract_method() {
-        let devnet = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
+        let devnet = BackgroundDevnet::spawn_with_additional_args(&["--state-archive-capacity", "full"]).await.expect("Could not start Devnet");
         let contract_address = FieldElement::from_hex_be(PREDEPLOYED_ACCOUNT_ADDRESS).unwrap();
         let entry_point_selector =
             starknet_rs_core::utils::get_selector_from_name("nonExistentMethod").unwrap();
@@ -53,6 +94,47 @@ mod call {
                     calldata: vec![contract_address],
                 },
                 BlockId::Tag(BlockTag::Latest),
+            )
+            .await
+            .expect_err("Should have failed");
+
+        match err {
+            ProviderError::StarknetError(StarknetError::ContractError(_)) => (),
+            _ => panic!("Invalid error: {err:?}"),
+        }
+
+        let block_hash=  devnet.create_block().await;
+        // Test with block hash
+        let err = devnet
+            .json_rpc_client
+            .call(
+                FunctionCall {
+                    contract_address: FieldElement::from_hex_be(ETH_ERC20_CONTRACT_ADDRESS)
+                        .unwrap(),
+                    entry_point_selector,
+                    calldata: vec![contract_address],
+                },
+                BlockId::Hash(block_hash.unwrap()),
+            )
+            .await
+            .expect_err("Should have failed");
+
+        match err {
+            ProviderError::StarknetError(StarknetError::ContractError(_)) => (),
+            _ => panic!("Invalid error: {err:?}"),
+        }
+
+        // Test with block number
+        let err = devnet
+            .json_rpc_client
+            .call(
+                FunctionCall {
+                    contract_address: FieldElement::from_hex_be(ETH_ERC20_CONTRACT_ADDRESS)
+                        .unwrap(),
+                    entry_point_selector,
+                    calldata: vec![contract_address],
+                },
+                BlockId::Number(1),
             )
             .await
             .expect_err("Should have failed");
