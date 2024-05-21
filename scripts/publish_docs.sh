2e4336fb2e5517152bad77f0918f3da512e4c452
FabijanC <fc@spaceshard.io>
2024-05-21 16:26:14 +0200

    Add dummy doc publishing

---
+++ scripts/publish_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd website && npm ci
+
+user="0xSpaceShard"
+git config --global user.email "$user@users.noreply.github.com"
+git config --global user.name "$user"
+
+# [skip ci] to avoid gh-pages erroring on CircleCI
+LATEST_COMMIT_HASH=$(git rev-parse HEAD)
+export CUSTOM_COMMIT_MESSAGE="Deploy website - based on $LATEST_COMMIT_HASH [skip ci]"
+export USE_SSH="true"
+npm run deploy
