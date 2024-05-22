94828700e895fd9b289e442555fd9c1077ee3392
FabijanC <fc@spaceshard.io>
2024-05-22 11:59:07 +0200

    Add comment to publish_docs.sh [skip ci]

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cd website && npm ci
 
 if [ "$CIRCLECI" = "true" ]; then
+    # if running on CircleCI, user info needs to be set up; locally you are assumed to have set it
     git config --global user.email "0xSpaceShard@users.noreply.github.com"
     git config --global user.name "0xSpaceShard"
 fi
