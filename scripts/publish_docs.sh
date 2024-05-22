77329dde7868d9e889601509d1b160a5cca9b4a6
FabijanC <fc@spaceshard.io>
2024-05-22 11:13:49 +0200

    Remove git global config in publish_docs.sh

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 
 cd website && npm ci
 
-user="0xSpaceShard"
-git config --global user.email "$user@users.noreply.github.com"
-git config --global user.name "$user"
-
 # [skip ci] to avoid deployment branch erroring on CircleCI
 LATEST_COMMIT_HASH=$(git rev-parse HEAD)
 export CUSTOM_COMMIT_MESSAGE="Deploy website - based on $LATEST_COMMIT_HASH [skip ci]"
