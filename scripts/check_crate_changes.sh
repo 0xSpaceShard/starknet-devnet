403f4080ef1e0a797b9b81ad5dcfe4bc35a2aed5
FabijanC <fc@spaceshard.io>
2024-06-10 13:53:50 +0200

    Release v0.0.7 (#490)

--- scripts/check_crate_changes.sh
+++ scripts/check_crate_changes.sh
@@ -3,7 +3,7 @@
 set -eu
 
 if [ $# -ne 1 ]; then
-    echo "$0: <VERSION>"
+    >&2 echo "$0: <VERSION>"
     exit 1
 fi
 
@@ -11,6 +11,6 @@ version="$1"
 
 echo "The crates that need a semver increment since git revision '$version' are:"
 
-git diff "$version" --stat | grep -o -E 'crates/[^/]*' | uniq
+git diff "$version" --name-status | grep -o -E 'crates/[^/]*' | uniq
 
 echo "Note that this does not reflect dependency changes in Cargo.toml or changes that one Devnet crate may have had on another!"
