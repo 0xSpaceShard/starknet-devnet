48c4705ca037b755bf5bf4766d802b0ee1db0d21
Tony Bo <61715244+tonypony220@users.noreply.github.com>
2024-06-27 08:29:38 +0200

    fix `starknet_getEvents` returning wildcard error on nonexistent block (#509)

--- crates/starknet-devnet/tests/test_get_events.rs
+++ crates/starknet-devnet/tests/test_get_events.rs
@@ -7,9 +7,9 @@ mod get_events_integration_tests {
         Account, Call, ConnectedAccount, ExecutionEncoding, SingleOwnerAccount,
     };
     use starknet_rs_contract::ContractFactory;
-    use starknet_rs_core::types::{BlockId, BlockTag, EventFilter, FieldElement};
+    use starknet_rs_core::types::{BlockId, BlockTag, EventFilter, FieldElement, StarknetError};
     use starknet_rs_core::utils::{get_selector_from_name, get_udc_deployed_address};
-    use starknet_rs_providers::Provider;
+    use starknet_rs_providers::{Provider, ProviderError};
 
     use crate::common::background_devnet::BackgroundDevnet;
     use crate::common::constants;
@@ -150,5 +150,47 @@ mod get_events_integration_tests {
                 .expect("Could not start Devnet");
 
         get_events_correct_chunking(&devnet, true).await
+    }
+
+    #[tokio::test]
+    async fn get_events_errors() {
+        let devnet = BackgroundDevnet::spawn().await.expect("Could not start Devnet");
+
+        {
+            let chunk_size: u64 = 3;
+            let continuation_token: Option<String> = None;
+            let event_filter = EventFilter {
+                from_block: Some(BlockId::Number(90000000)),
+                to_block: Some(BlockId::Tag(BlockTag::Latest)),
+                address: None,
+                keys: None,
+            };
+            match devnet
+                .json_rpc_client
+                .get_events(event_filter, continuation_token, chunk_size)
+                .await
+            {
+                Err(ProviderError::StarknetError(StarknetError::BlockNotFound)) => (),
+                resp => panic!("Unexpected response: {resp:?}"),
+            }
+        }
+        {
+            let chunk_size: u64 = 3;
+            let continuation_token = Some(String::from("invalid token"));
+            let event_filter = EventFilter {
+                from_block: Some(BlockId::Number(0)),
+                to_block: Some(BlockId::Tag(BlockTag::Latest)),
+                address: None,
+                keys: None,
+            };
+            match devnet
+                .json_rpc_client
+                .get_events(event_filter, continuation_token, chunk_size)
+                .await
+            {
+                Err(ProviderError::StarknetError(StarknetError::InvalidContinuationToken)) => (),
+                resp => panic!("Unexpected response: {resp:?}"),
+            }
+        }
     }
 }
