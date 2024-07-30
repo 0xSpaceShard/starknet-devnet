17b09cbe5d96dc6a88f42f058c5c34e17f4f7f35
marioiordanov <mio@shardlabs.io>
2024-07-30 11:55:33 +0300

    renaming to constants

--- crates/starknet-devnet-server/src/restrictive_mode.rs
+++ crates/starknet-devnet-server/src/restrictive_mode.rs
@@ -1,35 +1,71 @@
 use std::collections::HashMap;
 
+// Constants for URIs
+const URI_DUMP: &str = "/dump";
+const URI_LOAD: &str = "/load";
+const URI_SET_TIME: &str = "/set_time";
+const URI_INCREASE_TIME: &str = "/increase_time";
+const URI_CREATE_BLOCK: &str = "/create_block";
+const URI_ABORT_BLOCKS: &str = "/abort_blocks";
+const URI_RESTART: &str = "/restart";
+const URI_MINT: &str = "/mint";
+const URI_POSTMAN_LOAD_L1_MESSAGING_CONTRACT: &str = "/postman/load_l1_messaging_contract";
+const URI_POSTMAN_FLUSH: &str = "/postman/flush";
+const URI_POSTMAN_SEND_MESSAGE_TO_L2: &str = "/postman/send_message_to_l2";
+const URI_POSTMAN_CONSUME_MESSAGE_FROM_L2: &str = "/postman/consume_message_from_l2";
+const URI_PREDEPLOYED_ACCOUNTS: &str = "/predeployed_accounts";
+const URI_ACCOUNT_BALANCE: &str = "/account_balance";
+const URI_CONFIG: &str = "/config";
+
+// Constants for JSON RPC method names
+const RPC_METHOD_DUMP: &str = "devnet_dump";
+const RPC_METHOD_LOAD: &str = "devnet_load";
+const RPC_METHOD_SET_TIME: &str = "devnet_setTime";
+const RPC_METHOD_INCREASE_TIME: &str = "devnet_increaseTime";
+const RPC_METHOD_CREATE_BLOCK: &str = "devnet_createBlock";
+const RPC_METHOD_ABORT_BLOCKS: &str = "devnet_abortBlocks";
+const RPC_METHOD_RESTART: &str = "devnet_restart";
+const RPC_METHOD_MINT: &str = "devnet_mint";
+const RPC_METHOD_POSTMAN_LOAD: &str = "devnet_postmanLoad";
+const RPC_METHOD_POSTMAN_FLUSH: &str = "devnet_postmanFlush";
+const RPC_METHOD_POSTMAN_SEND_MESSAGE_TO_L2: &str = "devnet_postmanSendMessageToL2";
+const RPC_METHOD_POSTMAN_CONSUME_MESSAGE_FROM_L2: &str = "devnet_postmanConsumeMessageFromL2";
+const RPC_METHOD_GET_PREDEPLOYED_ACCOUNTS: &str = "devnet_getPredeployedAccounts";
+const RPC_METHOD_GET_ACCOUNT_BALANCE: &str = "devnet_getAccountBalance";
+const RPC_METHOD_GET_CONFIG: &str = "devnet_getConfig";
+const RPC_METHOD_IMPERSONATE_ACCOUNT: &str = "devnet_impersonateAccount";
+const RPC_METHOD_AUTO_IMPERSONATE: &str = "devnet_autoImpersonate";
+
 use lazy_static::lazy_static;
 lazy_static! {
     static ref HTTP_URI_TO_RPC_METHOD: HashMap<&'static str, String> = HashMap::from([
-        ("/dump", "devnet_dump".into()),
-        ("/load", "devnet_load".into()),
-        ("/set_time", "devnet_setTime".into()),
-        ("/increase_time", "devnet_increaseTime".into()),
-        ("/create_block", "devnet_createBlock".into()),
-        ("/abort_blocks", "devnet_abortBlocks".into()),
-        ("/restart", "devnet_restart".into()),
-        ("/mint", "devnet_mint".into()),
-        ("/postman/load_l1_messaging_contract", "devnet_postmanLoad".into()),
-        ("/postman/flush", "devnet_postmanFlush".into()),
-        ("/postman/send_message_to_l2", "devnet_postmanSendMessageToL2".into()),
-        ("/postman/consume_message_from_l2", "devnet_postmanConsumeMessageFromL2".into()),
-        ("/predeployed_accounts", "devnet_getPredeployedAccounts".into()),
-        ("/account_balance", "devnet_getAccountBalance".into()),
-        ("/config", "devnet_getConfig".into()),
+        (URI_DUMP, RPC_METHOD_DUMP.into()),
+        (URI_LOAD, RPC_METHOD_LOAD.into()),
+        (URI_SET_TIME, RPC_METHOD_SET_TIME.into()),
+        (URI_INCREASE_TIME, RPC_METHOD_INCREASE_TIME.into()),
+        (URI_CREATE_BLOCK, RPC_METHOD_CREATE_BLOCK.into()),
+        (URI_ABORT_BLOCKS, RPC_METHOD_ABORT_BLOCKS.into()),
+        (URI_RESTART, RPC_METHOD_RESTART.into()),
+        (URI_MINT, RPC_METHOD_MINT.into()),
+        (URI_POSTMAN_LOAD_L1_MESSAGING_CONTRACT, RPC_METHOD_POSTMAN_LOAD.into()),
+        (URI_POSTMAN_FLUSH, RPC_METHOD_POSTMAN_FLUSH.into()),
+        (URI_POSTMAN_SEND_MESSAGE_TO_L2, RPC_METHOD_POSTMAN_SEND_MESSAGE_TO_L2.into()),
+        (URI_POSTMAN_CONSUME_MESSAGE_FROM_L2, RPC_METHOD_POSTMAN_CONSUME_MESSAGE_FROM_L2.into()),
+        (URI_PREDEPLOYED_ACCOUNTS, RPC_METHOD_GET_PREDEPLOYED_ACCOUNTS.into()),
+        (URI_ACCOUNT_BALANCE, RPC_METHOD_GET_ACCOUNT_BALANCE.into()),
+        (URI_CONFIG, RPC_METHOD_GET_CONFIG.into()),
     ]);
     static ref RPC_METHOD_TO_HTTP_URI: HashMap<String, String> =
         HTTP_URI_TO_RPC_METHOD.iter().map(|(k, v)| (v.to_string(), String::from(*k))).collect();
     pub static ref DEFAULT_RESTRICTED_JSON_RPC_METHODS: Vec<String> = vec![
-        "devnet_mint".into(),
-        "devnet_load".into(),
-        "devnet_restart".into(),
-        "devnet_createBlock".into(),
-        "devnet_abortBlocks".into(),
-        "devnet_impersonateAccount".into(),
-        "devnet_autoImpersonate".into(),
-        "devnet_getPredeployedAccounts".into()
+        RPC_METHOD_MINT.into(),
+        RPC_METHOD_LOAD.into(),
+        RPC_METHOD_RESTART.into(),
+        RPC_METHOD_CREATE_BLOCK.into(),
+        RPC_METHOD_ABORT_BLOCKS.into(),
+        RPC_METHOD_IMPERSONATE_ACCOUNT.into(),
+        RPC_METHOD_AUTO_IMPERSONATE.into(),
+        RPC_METHOD_GET_PREDEPLOYED_ACCOUNTS.into()
     ];
 }
 
