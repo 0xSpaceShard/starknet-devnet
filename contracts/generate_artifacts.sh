ea2bbfe1348956a2425e7d6322791baf48b49331
marioiordanov <mio@shardlabs.io>
2024-09-19 15:32:41 +0300

    edited script

--- contracts/generate_artifacts.sh
+++ contracts/generate_artifacts.sh
@@ -12,8 +12,8 @@ cp ./l1-l2-messaging/solidity/out/L1L2.sol/L1L2Example.json ./l1-l2-artifacts/
 
 ## CAIRO
 scarb --manifest-path ./l1-l2-messaging/cairo/Scarb.toml build
-cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2.contract_class.json ./l1-l2-artifacts/
-cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2_lib.contract_class.sierra ./l1-l2-artifacts/
+cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2.contract_class.json ./l1-l2-artifacts/cairo_l1_l2.contract_class.sierra
+cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2_lib.contract_class.json ./l1-l2-artifacts/cairo_l1_l2_lib.contract_class.sierra
 
 cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2.contract_class.json ../crates/starknet-devnet/test_data/cairo1/messaging/cairo_1_l1l2.sierra
-cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2_lib.contract_class.sierra ../crates/starknet-devnet/test_data/cairo1/messaging/cairo_1_l1l2_lib.sierra
+cp ./l1-l2-messaging/cairo/target/dev/cairo_l1_l2_lib.contract_class.json ../crates/starknet-devnet/test_data/cairo1/messaging/cairo_1_l1l2_lib.sierra
