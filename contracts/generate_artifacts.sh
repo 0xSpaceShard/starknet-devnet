38b9f35a6e40f95db92539a1fa58d079c893299d
marioiordanov <mio@shardlabs.io>
2024-10-30 17:58:36 +0200

    edit generate_artifacts script to copy file to starknet-devnet-core crate [skip ci]

--- contracts/generate_artifacts.sh
+++ contracts/generate_artifacts.sh
@@ -3,12 +3,13 @@ set -eu
 
 # Bash script to generate the artifacts required by Devnet to compile.
 mkdir -p l1-l2-artifacts
+mkdir -p ../crates/starknet-devnet-core/contracts/l1-l2-artifacts
 
 # L1-L2 messaging:
 ## SOLIDITY
 forge build --root ./l1-l2-messaging/solidity
-cp ./l1-l2-messaging/solidity/out/MockStarknetMessaging.sol/MockStarknetMessaging.json ./l1-l2-artifacts/
 cp ./l1-l2-messaging/solidity/out/L1L2.sol/L1L2Example.json ./l1-l2-artifacts/
+cp ./l1-l2-messaging/solidity/out/MockStarknetMessaging.sol/MockStarknetMessaging.json ../crates/starknet-devnet-core/contracts/l1-l2-artifacts/
 
 ## CAIRO
 scarb --manifest-path ./l1-l2-messaging/cairo/Scarb.toml build
