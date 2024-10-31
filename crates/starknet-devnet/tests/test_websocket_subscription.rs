225a60115363449bc4d357775e0172ddd2b2c7c6
FabijanC <fc@spaceshard.io>
2024-10-31 16:30:08 +0100

    Test subscribing to old block [skip ci]

--- crates/starknet-devnet/tests/test_websocket_subscription.rs
+++ crates/starknet-devnet/tests/test_websocket_subscription.rs
@@ -20,7 +20,8 @@ mod websocket_subscription_support {
             send_text_rpc_via_ws(&mut ws, "starknet_subscribeNewHeads", json!({})).await.unwrap();
         let subscription_id = subscription_confirmation["result"].as_i64().unwrap();
 
-        for i in 0..2 {
+        // test with multiple blocks created, number 0 was origin, so we start at 1
+        for block_i in 1..=2 {
             let created_block_hash = devnet.create_block().await.unwrap();
 
             let notification = receive_rpc_via_ws(&mut ws).await.unwrap();
@@ -30,8 +31,7 @@ mod websocket_subscription_support {
                 created_block_hash.to_hex_string().as_str()
             );
 
-            // origin is 0, so shift by one
-            assert_eq!(notification["params"]["result"]["block_number"].as_i64().unwrap(), i + 1);
+            assert_eq!(notification["params"]["result"]["block_number"].as_i64().unwrap(), block_i);
             assert_eq!(
                 notification["params"]["subscription_id"].as_i64().unwrap(),
                 subscription_id
@@ -83,5 +83,42 @@ mod websocket_subscription_support {
                 subscription_id
             );
         }
+    }
+
+    #[tokio::test]
+    async fn subscription_to_an_older_block_should_notify_of_all_blocks_up_to_latest() {
+        let devnet = BackgroundDevnet::spawn().await.unwrap();
+        let (mut ws, _) = connect_async(devnet.ws_url()).await.unwrap();
+
+        let n_blocks = 5;
+        for _ in 0..n_blocks {
+            devnet.create_block().await.unwrap();
+        }
+
+        // request notifications for all blocks starting with genesis
+        let starting_block = 0;
+        let subscription_confirmation = send_text_rpc_via_ws(
+            &mut ws,
+            "starknet_subscribeNewHeads",
+            json!({ "block_id": { "block_number": starting_block } }),
+        )
+        .await
+        .unwrap();
+        let subscription_id = subscription_confirmation["result"].as_i64().unwrap();
+
+        for block_i in starting_block..=n_blocks {
+            let notification = receive_rpc_via_ws(&mut ws).await.unwrap();
+            assert_eq!(notification["method"], "starknet_subscriptionNewHeads");
+
+            assert_eq!(notification["params"]["result"]["block_number"].as_i64().unwrap(), block_i);
+            assert_eq!(
+                notification["params"]["subscription_id"].as_i64().unwrap(),
+                subscription_id
+            );
+        }
+
+        // assert no more messages to receive
+        let read_err = receive_rpc_via_ws(&mut ws).await.unwrap_err();
+        assert_contains(read_err.to_string().as_str(), "deadline has elapsed");
     }
 }
