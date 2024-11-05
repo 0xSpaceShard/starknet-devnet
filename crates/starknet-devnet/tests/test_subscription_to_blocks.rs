8821f68489cc743d7c7f067d36d4009924760b33
FabijanC <fc@spaceshard.io>
2024-11-05 10:32:13 +0100

    Expand testing to pending and block hash [skip ci]

--- crates/starknet-devnet/tests/test_subscription_to_blocks.rs
+++ crates/starknet-devnet/tests/test_subscription_to_blocks.rs
@@ -65,6 +65,8 @@ mod websocket_subscription_support {
             subscribers.insert(subscription_id, ws);
         }
 
+        assert_eq!(subscribers.len(), n_subscribers); // assert all IDs are different
+
         let created_block_hash = devnet.create_block().await.unwrap();
 
         for (subscription_id, mut ws) in subscribers {
@@ -84,7 +86,7 @@ mod websocket_subscription_support {
     }
 
     #[tokio::test]
-    async fn subscription_to_an_older_block_should_notify_of_all_blocks_up_to_latest() {
+    async fn subscription_to_an_old_block_by_number_should_notify_of_all_blocks_up_to_latest() {
         let devnet = BackgroundDevnet::spawn().await.unwrap();
         let (mut ws, _) = connect_async(devnet.ws_url()).await.unwrap();
 
@@ -119,6 +121,87 @@ mod websocket_subscription_support {
     }
 
     #[tokio::test]
+    async fn subscription_to_an_old_block_by_hash_should_notify_of_all_blocks_up_to_latest() {
+        let devnet = BackgroundDevnet::spawn().await.unwrap();
+        let (mut ws, _) = connect_async(devnet.ws_url()).await.unwrap();
+
+        let genesis_block = devnet.get_latest_block_with_tx_hashes().await.unwrap();
+
+        let n_blocks = 5;
+        for _ in 0..n_blocks {
+            devnet.create_block().await.unwrap();
+        }
+
+        // request notifications for all blocks starting with genesis
+        let starting_block = 0;
+        let subscription_confirmation = send_text_rpc_via_ws(
+            &mut ws,
+            "starknet_subscribeNewHeads",
+            json!({ "block_id": { "block_hash": genesis_block.block_hash } }),
+        )
+        .await
+        .unwrap();
+        let subscription_id = subscription_confirmation["result"].as_i64().unwrap();
+
+        for block_i in starting_block..=n_blocks {
+            let notification = receive_rpc_via_ws(&mut ws).await.unwrap();
+            assert_eq!(notification["method"], "starknet_subscriptionNewHeads");
+
+            assert_eq!(notification["params"]["result"]["block_number"].as_i64().unwrap(), block_i);
+            assert_eq!(
+                notification["params"]["subscription_id"].as_i64().unwrap(),
+                subscription_id
+            );
+        }
+
+        assert_no_notifications(&mut ws).await;
+    }
+
+    #[tokio::test]
+    async fn subscription_to_pending_block_is_same_as_latest() {
+        let devnet = BackgroundDevnet::spawn().await.unwrap();
+        let (mut ws_latest, _) = connect_async(devnet.ws_url()).await.unwrap();
+        let (mut ws_pending, _) = connect_async(devnet.ws_url()).await.unwrap();
+
+        // create two subscriptions: one to latest, one to pending
+        let subscription_latest = send_text_rpc_via_ws(
+            &mut ws_latest,
+            "starknet_subscribeNewHeads",
+            json!({ "block_id": "latest" }),
+        )
+        .await
+        .unwrap();
+        let subscription_id_latest = subscription_latest["result"].as_i64().unwrap();
+
+        let subscription_pending = send_text_rpc_via_ws(
+            &mut ws_pending,
+            "starknet_subscribeNewHeads",
+            json!({ "block_id": "pending" }),
+        )
+        .await
+        .unwrap();
+        let subscription_id_pending = subscription_pending["result"].as_i64().unwrap();
+
+        assert_ne!(subscription_id_latest, subscription_id_pending);
+
+        devnet.create_block().await.unwrap();
+
+        // assert notification equality after taking subscription IDs out
+        let mut notification_latest = receive_rpc_via_ws(&mut ws_latest).await.unwrap();
+        assert_eq!(notification_latest["params"]["subscription_id"].take(), subscription_id_latest);
+        assert_no_notifications(&mut ws_latest).await;
+
+        let mut notification_pending = receive_rpc_via_ws(&mut ws_pending).await.unwrap();
+        assert_eq!(
+            notification_pending["params"]["subscription_id"].take(),
+            subscription_id_pending
+        );
+        assert_no_notifications(&mut ws_pending).await;
+
+        assert_eq!(notification_latest, notification_pending);
+    }
+
+    #[tokio::test]
     async fn test_multiple_subscribers_one_unsubscribes() {
         let devnet = BackgroundDevnet::spawn().await.unwrap();
 
@@ -135,6 +218,8 @@ mod websocket_subscription_support {
             let subscription_id = subscription_confirmation["result"].as_i64().unwrap();
             subscribers.insert(subscription_id, ws);
         }
+
+        assert_eq!(subscribers.len(), n_subscribers); // assert all IDs are different
 
         // randomly choose one subscriber for unsubscription
         let mut unsubscriber_id = -1;
