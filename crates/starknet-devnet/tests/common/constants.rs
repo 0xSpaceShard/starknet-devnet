08a1b668c6d3f58a4ae33aa5d61614d800428632
FabijanC <fc@spaceshard.io>
2025-01-02 15:16:44 +0100

    Enable random port on --port 0 (#666)
    * More widespread use of HOST constant (change to 127.0.0.1)
    
    * Rely on system to handle port 0; apply port=0 in BackgroundDevnet
    
    * Refactor send_custom_rpc

--- crates/starknet-devnet/tests/common/constants.rs
+++ crates/starknet-devnet/tests/common/constants.rs
@@ -1,9 +1,7 @@
 use starknet_core::constants::DEVNET_DEFAULT_INITIAL_BALANCE;
 use starknet_rs_core::types::Felt;
 
-pub const HOST: &str = "localhost";
-pub const MIN_PORT: u16 = 1025;
-pub const MAX_PORT: u16 = 65_535;
+pub const HOST: &str = "127.0.0.1";
 pub const SEED: usize = 42;
 pub const ACCOUNTS: usize = 3;
 pub const CHAIN_ID: Felt = starknet_rs_core::chain_id::SEPOLIA;
