1241c8e8843101dbe33af3b387ce05ddcd2f1b73
marioiordanov <mio@shardlabs.io>
2025-01-09 14:43:16 +0200

    enhance clippy_check

--- scripts/clippy_check.sh
+++ scripts/clippy_check.sh
@@ -2,4 +2,9 @@
 
 set -eu
 
-cargo clippy --workspace --all-targets -- -Dwarnings
+# checks for errors in production code
+cargo clippy --workspace -- -Dwarnings
+# checks for errors in production code with enabled all features
+cargo clippy --workspace --all-features -- -Dwarnings
+# checks for errors in testing code
+cargo clippy --workspace --tests -- -Dwarnings
