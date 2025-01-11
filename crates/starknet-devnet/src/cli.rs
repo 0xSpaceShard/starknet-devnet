9d93777c1ec5b9b13336f294f30abab316867934
Fiiranek <jellybean@onet.eu>
2025-01-11 13:01:34 +0100

    Fix `--initial-balance` help text

--- crates/starknet-devnet/src/cli.rs
+++ crates/starknet-devnet/src/cli.rs
@@ -52,7 +52,9 @@ pub(crate) struct Args {
     #[arg(env = "ACCOUNT_CLASS_CUSTOM")]
     #[arg(value_name = "PATH")]
     #[arg(conflicts_with = "account_class_choice")]
-    #[arg(help = "Specify the path to a Cairo Sierra artifact to be used by predeployed accounts;")]
+    #[arg(
+        help = "Specify the path to a Cairo Sierra artifact to be used by predeployed accounts;"
+    )]
     account_class_custom: Option<AccountClassWrapper>,
 
     /// Initial balance of predeployed accounts
@@ -61,7 +63,7 @@ pub(crate) struct Args {
     #[arg(short = 'e')]
     #[arg(value_name = "DECIMAL_VALUE")]
     #[arg(default_value_t = InitialBalanceWrapper::default())]
-    #[arg(help = "Specify the initial balance in WEI of accounts to be predeployed;")]
+    #[arg(help = "Specify the initial balance in WEI and FRI of accounts to be predeployed;")]
     initial_balance: InitialBalanceWrapper,
 
     // Seed for predeployed accounts
@@ -85,7 +87,9 @@ pub(crate) struct Args {
     #[arg(env = "PORT")]
     #[arg(value_name = "PORT")]
     #[arg(default_value_t = DEVNET_DEFAULT_PORT)]
-    #[arg(help = "Specify the port to listen at; If 0, acquires a random free port and prints it;")]
+    #[arg(
+        help = "Specify the port to listen at; If 0, acquires a random free port and prints it;"
+    )]
     port: u16,
 
     // Set start time in seconds
