35a7209491b031b6b5e3ea6a9d3aa4bdde072675
FabijanC <fc@spaceshard.io>
2025-01-24 10:04:05 +0100

    Improve scripts/install_foundry.sh

--- scripts/install_foundry.sh
+++ scripts/install_foundry.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# If foundryup becomes buggy, consider switching to directly downloading binary from GitHub tag:
+# git_asset="https://github.com/foundry-rs/foundry/releases/download/$foundry_version/foundry_nightly_linux_amd64.tar.gz"
+# foundry_bin="$HOME/.foundry/bin"
+# mkdir -p "$foundry_bin"
+# curl -L "$git_asset" | tar -xvz -C "$foundry_bin"
+# export PATH="$PATH:$HOME/$foundry_bin"
+
 set -eu
 
-echo "Installing foundryup"
+foundry_version="nightly-5b7e4cb3c882b28f3c32ba580de27ce7381f415a"
+
+echo "Installing foundryup $foundry_version"
+
 curl -L https://foundry.paradigm.xyz | bash || echo "As expected, received a non-zero exit code"
 
 # make command available in PATH
@@ -13,7 +23,7 @@ if [ -n "$CIRCLE_BRANCH" ]; then
 fi
 
 echo "Installing foundry"
-foundryup --install nightly-5b7e4cb3c882b28f3c32ba580de27ce7381f415a
+foundryup --install "$foundry_version"
 
 # assert it works
 anvil --version
