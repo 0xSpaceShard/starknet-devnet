d2d5f6f13e58aadb41fa10de70ee782d177b62c6
FabijanC <fc@spaceshard.io>
2025-01-24 10:14:03 +0100

    Remove blockifier testing feature; use VersionedConstants 0.13.2 (#690)
    * Improve scripts/install_foundry.sh

--- scripts/install_foundry.sh
+++ scripts/install_foundry.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# If foundryup becomes buggy, consider switching to directly downloading binary from GitHub tag:
+# git_asset="https://github.com/foundry-rs/foundry/releases/download/$foundry_version/foundry_nightly_linux_amd64.tar.gz"
+# foundry_bin="$HOME/.foundry/bin"
+# mkdir -p "$foundry_bin"
+# curl -L "$git_asset" | tar -xvz -C "$foundry_bin"
+# export PATH="$PATH:$HOME/$foundry_bin"
+
 set -eu
+
+foundry_version="nightly-5b7e4cb3c882b28f3c32ba580de27ce7381f415a"
+
+echo "Installing foundry $foundry_version"
 
 echo "Installing foundryup"
 curl -L https://foundry.paradigm.xyz | bash || echo "As expected, received a non-zero exit code"
@@ -13,7 +24,7 @@ if [ -n "$CIRCLE_BRANCH" ]; then
 fi
 
 echo "Installing foundry"
-foundryup --install nightly-5b7e4cb3c882b28f3c32ba580de27ce7381f415a
+foundryup --install "$foundry_version"
 
 # assert it works
 anvil --version
