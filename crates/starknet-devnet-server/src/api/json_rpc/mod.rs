129226c6e1f74d037b8bb5cf89438a8349277a48
marioiordanov <mio@shardlabs.io>
2025-02-17 12:01:23 +0200

    updated json-rpc spec

--- crates/starknet-devnet-server/src/api/json_rpc/mod.rs
+++ crates/starknet-devnet-server/src/api/json_rpc/mod.rs
@@ -7,7 +7,7 @@ pub(crate) mod origin_forwarder;
 mod spec_reader;
 mod write_endpoints;
 
-pub const RPC_SPEC_VERSION: &str = "0.8.0-rc.2";
+pub const RPC_SPEC_VERSION: &str = "0.8.0-rc3";
 
 use std::sync::Arc;
 
@@ -51,8 +51,9 @@ use self::models::{
     SyncingOutput,
 };
 use self::origin_forwarder::OriginForwarder;
-use super::http::endpoints::accounts::{BalanceQuery, PredeployedAccountsQuery};
+use super::Api;
 use super::http::endpoints::DevnetConfig;
+use super::http::endpoints::accounts::{BalanceQuery, PredeployedAccountsQuery};
 use super::http::models::{
     AbortedBlocks, AbortingBlocks, AccountBalanceResponse, CreatedBlock, DumpPath,
     DumpResponseBody, FlushParameters, FlushedMessages, IncreaseTime, IncreaseTimeResponse,
@@ -60,7 +61,7 @@ use super::http::models::{
     PostmanLoadL1MessagingContract, RestartParameters, SerializableAccount, SetTime,
     SetTimeResponse,
 };
-use super::Api;
+use crate::ServerConfig;
 use crate::api::json_rpc::models::{
     BroadcastedDeclareTransactionEnumWrapper, BroadcastedDeployAccountTransactionEnumWrapper,
     BroadcastedInvokeTransactionEnumWrapper, SimulateTransactionsInput,
@@ -76,7 +77,6 @@ use crate::subscribe::{
     NewTransactionStatus, NotificationData, PendingTransactionNotification, SocketId,
     TransactionHashWrapper,
 };
-use crate::ServerConfig;
 
 /// Helper trait to easily convert results to rpc results
 pub trait ToRpcResponseResult {
@@ -1698,8 +1698,8 @@ mod requests_tests {
 
 #[cfg(test)]
 mod response_tests {
-    use crate::api::json_rpc::error::StrictRpcResult;
     use crate::api::json_rpc::ToRpcResponseResult;
+    use crate::api::json_rpc::error::StrictRpcResult;
 
     #[test]
     fn serializing_starknet_response_empty_variant_yields_empty_json_on_conversion_to_rpc_result() {
