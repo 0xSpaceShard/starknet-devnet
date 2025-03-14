d244840a89a2d190dfafc7d0658656f65f740081
marioiordanov <mio@shardlabs.io>
2025-03-14 23:30:18 +0200

    changes to error structure

--- crates/starknet-devnet-core/src/error.rs
+++ crates/starknet-devnet-core/src/error.rs
@@ -1,3 +1,6 @@
+use std::fmt::format;
+
+use blockifier::execution::stack_trace::{ErrorStackSegment, gen_tx_execution_error_trace};
 use blockifier::fee::fee_checks::FeeCheckError;
 use blockifier::transaction::errors::{
     TransactionExecutionError, TransactionFeeError, TransactionPreValidationError,
@@ -8,8 +11,6 @@ use starknet_types::contract_address::ContractAddress;
 use starknet_types::contract_storage_key::ContractStorageKey;
 use thiserror::Error;
 
-use crate::stack_trace::{gen_tx_execution_error_trace, ErrorStack};
-
 #[derive(Error, Debug)]
 pub enum Error {
     #[error(transparent)]
@@ -19,9 +20,12 @@ pub enum Error {
     #[error(transparent)]
     BlockifierStateError(#[from] blockifier::state::errors::StateError),
     #[error("{0:?}")]
-    ContractExecutionError(ErrorStack),
-    #[error("Execution error in simulating transaction no. {failure_index}: {error_stack:?}")]
-    ContractExecutionErrorInSimulation { failure_index: usize, error_stack: ErrorStack },
+    ContractExecutionError(ContractExecutionError),
+    #[error("Execution error in simulating transaction no. {failure_index}: {execution_error:?}")]
+    ContractExecutionErrorInSimulation {
+        failure_index: usize,
+        execution_error: ContractExecutionError,
+    },
     #[error("Types error: {0}")]
     TypesError(#[from] starknet_types::error::Error),
     #[error("I/O error: {0}")]
@@ -129,7 +133,7 @@ impl From<TransactionExecutionError> for Error {
                 TransactionValidationError::ValidationFailure { reason: panic_reason.to_string() }
                     .into()
             }
-            other => Self::ContractExecutionError(gen_tx_execution_error_trace(&other)),
+            other => Self::ContractExecutionError(gen_tx_execution_error_trace(&other).into()),
         }
     }
 }
@@ -183,3 +187,375 @@ pub enum MessagingError {
 }
 
 pub type DevnetResult<T, E = Error> = Result<T, E>;
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct InnerContractExecutionError {
+    pub contract_address: starknet_api::core::ContractAddress,
+    pub class_hash: Felt,
+    pub selector: Felt,
+    #[serde(skip)]
+    pub return_data: Retdata,
+    pub error: Box<ContractExecutionError>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ContractExecutionError {
+    /// Nested contract call stack trace frame.
+    Nested(InnerContractExecutionError),
+    /// Terminal error message.
+    Message(String),
+}
+
+impl From<String> for ContractExecutionError {
+    fn from(value: String) -> Self {
+        ContractExecutionError::Message(value)
+    }
+}
+
+use blockifier::{
+    execution::call_info::{self, CallInfo, Retdata},
+    transaction::objects::TransactionExecutionInfo,
+};
+use serde::{Deserialize, Serialize};
+use starknet_rs_core::utils::parse_cairo_short_string;
+
+impl From<blockifier::execution::stack_trace::ErrorStack> for ContractExecutionError {
+    fn from(error_stack: blockifier::execution::stack_trace::ErrorStack) -> Self {
+        let mut recursive_error_option = Option::<ContractExecutionError>::None;
+
+        // [[[error inner] error outer] root]
+
+        for frame in error_stack.stack.iter().rev() {
+            let stack_err = match frame {
+                ErrorStackSegment::Cairo1RevertSummary(revert_summary) => {
+                    let mut recursive_error = ContractExecutionError::Message(
+                        serde_json::to_string(&revert_summary.last_retdata.0).unwrap_or_default(),
+                    );
+
+                    for trace in revert_summary.stack.iter().rev() {
+                        recursive_error =
+                            ContractExecutionError::Nested(InnerContractExecutionError {
+                                contract_address: trace.contract_address,
+                                class_hash: trace.class_hash.unwrap_or_default().0,
+                                selector: trace.selector.0,
+                                return_data: revert_summary.last_retdata.clone(),
+                                error: Box::new(recursive_error),
+                            });
+                    }
+
+                    recursive_error
+                }
+                ErrorStackSegment::Vm(vm) => ContractExecutionError::Message(vm.into()),
+                ErrorStackSegment::StringFrame(msg) => ContractExecutionError::Message(msg.clone()),
+                ErrorStackSegment::EntryPoint(entry_point_error_frame) => {
+                    let error_reason = match entry_point_error_frame.preamble_type {
+                        blockifier::execution::stack_trace::PreambleType::CallContract => {
+                            "Error in external contract"
+                        }
+                        blockifier::execution::stack_trace::PreambleType::LibraryCall => {
+                            "Error in library call "
+                        }
+                        blockifier::execution::stack_trace::PreambleType::Constructor => {
+                            "Error in constructor"
+                        }
+                    };
+
+                    let error = recursive_error_option.take().unwrap_or_else(|| {
+                        ContractExecutionError::Message(error_reason.to_string())
+                    });
+
+                    ContractExecutionError::Nested(InnerContractExecutionError {
+                        contract_address: entry_point_error_frame.storage_address,
+                        class_hash: entry_point_error_frame.class_hash.0,
+                        selector: entry_point_error_frame.selector.unwrap_or_default().0,
+                        return_data: Retdata(Vec::new()),
+                        error: Box::new(error),
+                    })
+                }
+            };
+
+            recursive_error_option = Some(stack_err);
+        }
+
+        if let Some(recursive_error) = recursive_error_option {
+            recursive_error
+        } else {
+            let error_msg = match error_stack.header {
+                blockifier::execution::stack_trace::ErrorStackHeader::Constructor => {
+                    "Constructor error"
+                }
+                blockifier::execution::stack_trace::ErrorStackHeader::Execution => {
+                    "Execution error"
+                }
+                blockifier::execution::stack_trace::ErrorStackHeader::Validation => {
+                    "Validation error"
+                }
+                blockifier::execution::stack_trace::ErrorStackHeader::None => "Unknown error",
+            };
+
+            ContractExecutionError::Message(error_msg.to_string())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use blockifier::{
+        execution::{
+            call_info::{self, CallInfo, Retdata},
+            stack_trace::{ErrorStack, ErrorStackSegment},
+        },
+        transaction::objects::TransactionExecutionInfo,
+    };
+    use serde::{Deserialize, Serialize};
+    use starknet_api::core::ContractAddress;
+    use starknet_rs_core::utils::parse_cairo_short_string;
+    use starknet_types_core::felt::Felt;
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct ContractErrorData {
+        /// The execution trace up to the point of failure
+        pub revert_error: ContractExecutionError,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct InnerContractExecutionError {
+        pub contract_address: ContractAddress,
+        pub class_hash: Felt,
+        pub selector: Felt,
+        #[serde(skip)]
+        pub return_data: Retdata,
+        pub error: Box<ContractExecutionError>,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    #[serde(untagged)]
+    pub enum ContractExecutionError {
+        /// Nested contract call stack trace frame.
+        Nested(InnerContractExecutionError),
+        /// Terminal error message.
+        Message(String),
+    }
+
+    impl From<ErrorStack> for ContractExecutionError {
+        fn from(error_stack: ErrorStack) -> Self {
+            let mut recursive_error_option = Option::<ContractExecutionError>::None;
+
+            // [[[error inner] error outer] root]
+
+            for frame in error_stack.stack.iter().rev() {
+                let stack_err = match frame {
+                    ErrorStackSegment::Cairo1RevertSummary(revert_summary) => {
+                        let mut recursive_error = ContractExecutionError::Message(
+                            serde_json::to_string(&revert_summary.last_retdata).unwrap_or_default(),
+                        );
+
+                        for trace in revert_summary.stack.iter().rev() {
+                            recursive_error =
+                                ContractExecutionError::Nested(InnerContractExecutionError {
+                                    contract_address: trace.contract_address,
+                                    class_hash: trace.class_hash.unwrap_or_default().0,
+                                    selector: trace.selector.0,
+                                    return_data: revert_summary.last_retdata.clone(),
+                                    error: Box::new(recursive_error),
+                                });
+                        }
+
+                        recursive_error
+                    }
+                    ErrorStackSegment::Vm(vm) => ContractExecutionError::Message(vm.into()),
+                    ErrorStackSegment::StringFrame(msg) => {
+                        ContractExecutionError::Message(msg.clone())
+                    }
+                    ErrorStackSegment::EntryPoint(entry_point_error_frame) => {
+                        let error_reason = match entry_point_error_frame.preamble_type {
+                            blockifier::execution::stack_trace::PreambleType::CallContract => {
+                                "Error in external contract"
+                            }
+                            blockifier::execution::stack_trace::PreambleType::LibraryCall => {
+                                "Error in library call "
+                            }
+                            blockifier::execution::stack_trace::PreambleType::Constructor => {
+                                "Error in constructor"
+                            }
+                        };
+
+                        let error = recursive_error_option.take().unwrap_or_else(|| {
+                            ContractExecutionError::Message(error_reason.to_string())
+                        });
+
+                        ContractExecutionError::Nested(InnerContractExecutionError {
+                            contract_address: entry_point_error_frame.storage_address,
+                            class_hash: entry_point_error_frame.class_hash.0,
+                            selector: entry_point_error_frame.selector.unwrap_or_default().0,
+                            return_data: Retdata(Vec::new()),
+                            error: Box::new(error),
+                        })
+                    }
+                };
+
+                recursive_error_option = Some(stack_err);
+            }
+
+            if let Some(recursive_error) = recursive_error_option {
+                recursive_error
+            } else {
+                let error_msg = match error_stack.header {
+                    blockifier::execution::stack_trace::ErrorStackHeader::Constructor => {
+                        "Constructor error"
+                    }
+                    blockifier::execution::stack_trace::ErrorStackHeader::Execution => {
+                        "Execution error"
+                    }
+                    blockifier::execution::stack_trace::ErrorStackHeader::Validation => {
+                        "Validation error"
+                    }
+                    blockifier::execution::stack_trace::ErrorStackHeader::None => "Unknown error",
+                };
+
+                ContractExecutionError::Message(error_msg.to_string())
+            }
+        }
+    }
+
+    fn collect_failed_calls(root_call: &CallInfo) -> Vec<&CallInfo> {
+        let mut calls = vec![];
+
+        for inner_call in root_call.inner_calls.iter() {
+            calls.extend(collect_failed_calls(inner_call));
+        }
+
+        if root_call.execution.failed {
+            calls.push(root_call);
+        }
+
+        calls
+    }
+
+    // impl From<CallInfo> for ContractExecutionError {
+    //     fn from(value: CallInfo) -> Self {}
+    // }
+
+    #[test]
+    fn des_snrs() {
+        let js = r#"{
+  "call": {
+    "class_hash": "0x95fbeefe3200091edaeda57a08d76357e04d1a1ca878a4d1af777b9ce69af1",
+    "code_address": null,
+    "entry_point_type": "EXTERNAL",
+    "entry_point_selector": "0x1017d0207c04f7ba93b6432c22f38693c884e40738af93551399097b24d0f27",
+    "calldata": [
+      "0x231560ec8712d5fd115eceb2be40b7186da134a32e2bc966fc63e5da48a72c2",
+      "0x66756e6e795f74657874"
+    ],
+    "storage_address": "0x7500599919bcdd583f7ab259fdecddfe91df7b188aabd359652036bd27c671f",
+    "caller_address": "0x0",
+    "call_type": "Call",
+    "initial_gas": 10000000000
+  },
+  "execution": {
+    "retdata": [
+      "0x526573756c743a3a756e77726170206661696c65642e"
+    ],
+    "events": [],
+    "l2_to_l1_messages": [],
+    "failed": true,
+    "gas_consumed": 0
+  },
+  "inner_calls": [
+    {
+      "call": {
+        "class_hash": "0x95fbeefe3200091edaeda57a08d76357e04d1a1ca878a4d1af777b9ce69af1",
+        "code_address": "0x231560ec8712d5fd115eceb2be40b7186da134a32e2bc966fc63e5da48a72c2",
+        "entry_point_type": "EXTERNAL",
+        "entry_point_selector": "0x2f30ca48a88216d700251f5f06cb6dbdc3420bdef67879ba6836b55cf0d0dfd",
+        "calldata": [
+          "0x66756e6e795f74657874"
+        ],
+        "storage_address": "0x231560ec8712d5fd115eceb2be40b7186da134a32e2bc966fc63e5da48a72c2",
+        "caller_address": "0x7500599919bcdd583f7ab259fdecddfe91df7b188aabd359652036bd27c671f",
+        "call_type": "Call",
+        "initial_gas": 10000000000
+      },
+      "execution": {
+        "retdata": [
+          "0x66756e6e795f74657874"
+        ],
+        "events": [],
+        "l2_to_l1_messages": [],
+        "failed": true,
+        "gas_consumed": 0
+      },
+      "inner_calls": [],
+      "resources": {
+        "n_steps": 31,
+        "n_memory_holes": 0,
+        "builtin_instance_counter": {
+          "range_check_builtin": 2
+        }
+      },
+      "tracked_resource": "CairoSteps",
+      "storage_read_values": [],
+      "accessed_storage_keys": [],
+      "read_class_hash_values": [],
+      "accessed_contract_addresses": []
+    }
+  ],
+  "resources": {
+    "n_steps": 960,
+    "n_memory_holes": 2,
+    "builtin_instance_counter": {
+      "range_check_builtin": 22
+    }
+  },
+  "tracked_resource": "CairoSteps",
+  "storage_read_values": [],
+  "accessed_storage_keys": [],
+  "read_class_hash_values": [],
+  "accessed_contract_addresses": []
+}"#;
+
+        let call_info: CallInfo = serde_json::from_str(js).unwrap();
+
+        let failed_calls = collect_failed_calls(&call_info);
+
+        let combined_error_msg = Box::new(ContractExecutionError::Message(
+            serde_json::to_string(
+                &failed_calls
+                    .iter()
+                    .rev()
+                    .flat_map(|f| f.execution.retdata.clone().0)
+                    .collect::<Vec<Felt>>(),
+            )
+            .unwrap_or_default(),
+        ));
+
+        let mut recursive_error = Option::<Box<ContractExecutionError>>::None;
+        for f in failed_calls {
+            let current = if let Some(last_failed) = recursive_error.take() {
+                InnerContractExecutionError {
+                    contract_address: f.call.storage_address,
+                    class_hash: f.call.class_hash.unwrap_or_default().0,
+                    selector: f.call.entry_point_selector.0,
+                    return_data: f.execution.retdata.clone(),
+                    error: last_failed,
+                }
+            } else {
+                InnerContractExecutionError {
+                    contract_address: f.call.storage_address,
+                    class_hash: f.call.class_hash.unwrap_or_default().0,
+                    selector: f.call.entry_point_selector.0,
+                    return_data: f.execution.retdata.clone(),
+                    error: combined_error_msg.clone(),
+                }
+            };
+
+            recursive_error = Some(Box::new(ContractExecutionError::Nested(current)));
+        }
+
+        println!("{}", serde_json::to_string(&recursive_error.unwrap()).unwrap());
+    }
+}
