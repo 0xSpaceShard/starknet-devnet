47269b4222a0f94d15d13599e2933ba3cc28abb7
FabijanC <fc@spaceshard.io>
2025-03-17 17:11:26 +0100

    Reinstante test_subscription_to_events [skip ci]

--- tests/integration/lib.rs
+++ tests/integration/lib.rs
@@ -44,7 +44,7 @@ mod test_fork;
 
 // Fabijan
 mod test_subscription_to_blocks;
-// mod test_subscription_to_events;
+mod test_subscription_to_events;
 // mod test_subscription_to_pending_txs;
 // mod test_subscription_to_reorg;
 // mod test_subscription_to_tx_status;
