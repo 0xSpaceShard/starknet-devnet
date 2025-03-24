30f52bdb2bc50a8d8db0bb379f7f4681b322a1e5
FabijanC <fc@spaceshard.io>
2025-03-24 12:09:33 +0100

    Reinstate test_transaction_handling [skip ci]

--- tests/integration/lib.rs
+++ tests/integration/lib.rs
@@ -24,7 +24,7 @@ mod test_old_state; // TODO
 mod test_restart;
 
 // mod test_v3_transactions;
-// mod test_transaction_handling;
+mod test_transaction_handling;
 
 mod test_trace;
 mod test_restrictive_mode;
