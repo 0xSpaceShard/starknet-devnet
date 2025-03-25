ef008ed6799a351c8628666f4d7ad1970316241a
FabijanC <fc@spaceshard.io>
2025-03-25 16:59:07 +0100

    Improve test_old_state test

--- crates/starknet-devnet-core/src/error.rs
+++ crates/starknet-devnet-core/src/error.rs
@@ -266,8 +266,8 @@ impl From<(blockifier::execution::stack_trace::ErrorStack, String)> for Contract
                     recursive_error
                 }
 
-                // VMException frame is omitted, unless its the last frame of the error stack. It
-                // doesnt produce any meaningful message to the developer
+                // VMException frame is omitted, unless it's the last frame of the error stack. It
+                // doesn't produce any meaningful message to the developer.
                 ErrorStackSegment::Vm(vm) => recursive_error_option.take().unwrap_or(
                     ContractExecutionError::Message(format_error(&error_string, &String::from(vm))),
                 ),
