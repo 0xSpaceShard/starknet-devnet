e186483ba53efb683a716e315f99dbd62474d60b
FabijanC <fc@spaceshard.io>
2025-04-02 12:42:12 +0200

    Add too_many_blocks_behind tests [skip ci]

--- tests/integration/test_subscription_with_invalid_block_id.rs
+++ tests/integration/test_subscription_with_invalid_block_id.rs
@@ -1,9 +1,13 @@
 use serde_json::json;
 use starknet_rs_core::types::{BlockId, Felt};
+use starknet_rs_providers::Provider;
 use tokio_tungstenite::connect_async;
 
 use crate::common::background_devnet::BackgroundDevnet;
+use crate::common::constants::MAINNET_URL;
 use crate::common::utils::{assert_no_notifications, send_text_rpc_via_ws};
+
+const MAXIMUM_BLOCKS_BEHIND: u64 = 1024;
 
 fn block_not_found_error() -> serde_json::Value {
     json!({ "jsonrpc": "2.0", "id": 0, "error": { "code": 24, "message": "Block not found" } })
@@ -71,3 +75,63 @@ async fn test_pending_block_not_allowed_in_block_and_event_subscription() {
         assert_eq!(subscription_resp, call_on_pending_error(), "Method: {subscription_method}");
     }
 }
+
+#[tokio::test]
+async fn test_subscribing_to_too_many_blocks_behind() {
+    let forked_devnet =
+        BackgroundDevnet::spawn_with_additional_args(&["--fork-network", MAINNET_URL])
+            .await
+            .unwrap();
+
+    let (mut ws, _) = connect_async(forked_devnet.ws_url()).await.unwrap();
+
+    let latest_block_number = forked_devnet.json_rpc_client.block_number().await.unwrap();
+
+    let too_old_block_id =
+        json!({"block_id": { "block_number": latest_block_number - MAXIMUM_BLOCKS_BEHIND - 1 }});
+
+    for subscription_method in ["starknet_subscribeNewHeads", "starknet_subscribeEvents"] {
+        let subscription_resp =
+            send_text_rpc_via_ws(&mut ws, subscription_method, too_old_block_id.clone())
+                .await
+                .unwrap();
+
+        assert_eq!(
+            subscription_resp,
+            json!({
+                "jsonrpc": "2.0",
+                "id": 0,
+                "error": {
+                    "code": 40,
+                    "message": "Too many blocks behind",
+                },
+            }),
+            "Method: {subscription_method}"
+        );
+    }
+}
+
+#[tokio::test]
+async fn test_subscribing_to_maximum_blocks_behind() {
+    let forked_devnet =
+        BackgroundDevnet::spawn_with_additional_args(&["--fork-network", MAINNET_URL])
+            .await
+            .unwrap();
+
+    let (mut ws, _) = connect_async(forked_devnet.ws_url()).await.unwrap();
+
+    let latest_block_number = forked_devnet.json_rpc_client.block_number().await.unwrap();
+
+    let maximum_block_id =
+        json!({"block_id": { "block_number": latest_block_number - MAXIMUM_BLOCKS_BEHIND }});
+
+    for subscription_method in ["starknet_subscribeNewHeads", "starknet_subscribeEvents"] {
+        let _subscription_id =
+            send_text_rpc_via_ws(&mut ws, subscription_method, maximum_block_id.clone())
+                .await
+                .unwrap();
+    }
+}
+
+// TODO consider moving these tests to their respective test files. Having error cases inside a
+// separate file is dubious.
