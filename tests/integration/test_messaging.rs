36ed61151504baa74ccd43bd983b931aa2fbdf21
Sanchet Nagarnaik <130159643+nagxsan@users.noreply.github.com>
2025-04-14 13:55:30 +0530

    Custom account in deploying L1-L2 messaging contract (#740)
    * [skip ci]
    
    * feat: add private key parameter in loading l1 contract
    
    * replace dummy private key
    
    * update docs for postman load contract
    
    * add incorrect private key tests
    
    * formatting changes
    
    * Improve predeclaration/predeployment startup log (#734)
    
    * Abstract public+private key as KeyPair: reduce number of fn args
    
    * fix tests revert anvil changes
    
    * update incorrect test assert code
    
    ---------
    
    Co-authored-by: FabijanC <fc@spaceshard.io>

--- tests/integration/test_messaging.rs
+++ tests/integration/test_messaging.rs
@@ -29,7 +29,8 @@ use starknet_rs_signers::LocalWallet;
 use crate::common::background_anvil::BackgroundAnvil;
 use crate::common::background_devnet::BackgroundDevnet;
 use crate::common::constants::{
-    CHAIN_ID, L1_HANDLER_SELECTOR, MESSAGING_L1_CONTRACT_ADDRESS, MESSAGING_L2_CONTRACT_ADDRESS,
+    CHAIN_ID, DEFAULT_ETH_ACCOUNT_ADDRESS, DEFAULT_ETH_ACCOUNT_PRIVATE_KEY, L1_HANDLER_SELECTOR,
+    MESSAGING_L1_CONTRACT_ADDRESS, MESSAGING_L2_CONTRACT_ADDRESS,
     MESSAGING_WHITELISTED_L1_CONTRACT,
 };
 use crate::common::errors::RpcError;
@@ -338,6 +339,44 @@ async fn can_deploy_l1_messaging_contract() {
 
     let body = devnet
         .send_custom_rpc("devnet_postmanLoad", json!({ "network_url": anvil.url }))
+        .await
+        .expect("deploy l1 messaging contract failed");
+
+    assert_eq!(
+        body.get("messaging_contract_address").unwrap().as_str().unwrap(),
+        MESSAGING_L1_ADDRESS
+    );
+}
+
+#[tokio::test]
+async fn setup_anvil_incorrect_eth_private_key() {
+    let anvil = BackgroundAnvil::spawn_with_additional_args(&["--balance", "0"]).await.unwrap();
+
+    let (devnet, _, _) = setup_devnet(&["--account-class", "cairo1"]).await;
+
+    let body = devnet
+        .send_custom_rpc(
+            "devnet_postmanLoad",
+            json!({ "network_url": anvil.url,
+            "funded_account_private_key": DEFAULT_ETH_ACCOUNT_PRIVATE_KEY }),
+        )
+        .await
+        .unwrap_err();
+    assert_contains(&body.message, "Out of gas: gas required exceeds allowance: 0");
+}
+
+#[tokio::test]
+async fn deploy_l1_messaging_contract_with_funded_account_private_key() {
+    let anvil = BackgroundAnvil::spawn().await.unwrap();
+    assert_eq!(
+        anvil.provider_signer.address(),
+        DEFAULT_ETH_ACCOUNT_ADDRESS.parse::<Address>().unwrap()
+    );
+
+    let (devnet, _, _) = setup_devnet(&["--account-class", "cairo1"]).await;
+
+    let body = devnet
+        .send_custom_rpc("devnet_postmanLoad", json!({ "network_url": anvil.url, "funded_account_private_key": DEFAULT_ETH_ACCOUNT_PRIVATE_KEY }))
         .await
         .expect("deploy l1 messaging contract failed");
 
