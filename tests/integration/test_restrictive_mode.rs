31382d3d8dffc172e77cc89524c91943234ab09e
FabijanC <fc@spaceshard.io>
2025-07-25 14:11:20 +0200

    Remove non-rpc logic from integration tests

--- tests/integration/test_restrictive_mode.rs
+++ tests/integration/test_restrictive_mode.rs
@@ -2,7 +2,7 @@ use serde_json::json;
 
 use crate::common::background_devnet::BackgroundDevnet;
 use crate::common::errors::RpcError;
-use crate::common::reqwest_client::{GetReqwestSender, HttpEmptyResponseBody, PostReqwestSender};
+use crate::common::reqwest_client::{GetReqwestSender, HttpEmptyResponseBody};
 
 #[tokio::test]
 async fn restrictive_mode_with_default_methods_doesnt_affect_other_functionality() {
@@ -23,13 +23,6 @@ async fn restrictive_mode_with_default_methods() {
     let devnet = BackgroundDevnet::spawn_with_additional_args(&["--restrictive-mode"])
         .await
         .expect("Could not start Devnet");
-    let http_err = devnet
-        .reqwest_client()
-        .post_json_async("/mint", json!({ "address": "0x1", "amount": 1 }))
-        .await
-        .map(|_: HttpEmptyResponseBody| ())
-        .unwrap_err();
-    assert_eq!(http_err.status(), reqwest::StatusCode::FORBIDDEN);
 
     let json_rpc_error = devnet
         .send_custom_rpc("devnet_mint", json!({ "address": "0x1", "amount": 1 }))
@@ -46,19 +39,11 @@ async fn restrictive_mode_with_default_methods() {
 async fn restrictive_mode_with_custom_methods() {
     let devnet = BackgroundDevnet::spawn_with_additional_args(&[
         "--restrictive-mode",
-        "/load",
+        "devnet_load",
         "devnet_mint",
     ])
     .await
     .expect("Could not start Devnet");
-    let err = devnet
-        .reqwest_client()
-        .post_json_async("/load", json!({ "path": "dummy_path" }))
-        .await
-        .map(|_: HttpEmptyResponseBody| ())
-        .unwrap_err();
-
-    assert_eq!(err.status(), reqwest::StatusCode::FORBIDDEN);
 
     let json_rpc_error = devnet
         .send_custom_rpc("devnet_mint", json!({ "address": "0x1", "amount": 1 }))
