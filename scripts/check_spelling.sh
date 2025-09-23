43c4933ef656fbf595f74147bf6b0d90437e6d0d
FabijanC <fc@spaceshard.io>
2025-09-23 09:27:21 +0200

    Use `--locked` with `cargo install` (#856)

--- scripts/check_spelling.sh
+++ scripts/check_spelling.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 # should skip if already installed
-cargo +nightly-2025-02-20 install typos-cli --version 1.36.2
+cargo +nightly-2025-02-20 install typos-cli --version 1.36.2 --locked
 
 typos && echo "No spelling errors!"
