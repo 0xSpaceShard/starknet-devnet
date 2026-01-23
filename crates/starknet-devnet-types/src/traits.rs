850d81c83dcbc977fca41bc77b2b56e2f87658c8
3alpha <lg@shardlabs.io>
2026-01-22 23:03:37 -0300

    Implement TryHashProducer for HashProducer

--- crates/starknet-devnet-types/src/traits.rs
+++ crates/starknet-devnet-types/src/traits.rs
@@ -8,3 +8,10 @@ pub trait TryHashProducer {
 pub trait HashProducer {
     fn generate_hash(&self) -> Felt;
 }
+
+impl<T: HashProducer> TryHashProducer for T {
+    type Error = ();
+    fn try_generate_hash(&self) -> Result<Felt, Self::Error> {
+        Ok(self.generate_hash())
+    }
+}
